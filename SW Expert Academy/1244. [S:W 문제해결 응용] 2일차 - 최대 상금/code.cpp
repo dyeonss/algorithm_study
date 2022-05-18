895311b85dc84934cd4d4b4a056e1e5512fa2857
shindoyeon <dyeon7310@gmail.com>
2022-05-18 18:20:19 +0900

    1244번 재귀함수 이용

--- SW Expert Academy/1244. [S:W 문제해결 응용] 2일차 - 최대 상금/code.cpp
+++ SW Expert Academy/1244. [S:W 문제해결 응용] 2일차 - 최대 상금/code.cpp
@@ -8,81 +8,51 @@
  * @copyright Copyright (c) 2022
  * 
  */
-#include<iostream>
-#include <algorithm>
+#include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
-vector<int> makeNumArray(int x){
-    vector<int> arr;
-    int i=0;
+int ans, change;
+string s;
 
-    while(x>0){
-        arr.push_back(x%10);
-        x=x/10;
-        i++;
-    }
-    reverse(arr.begin(), arr.end());
-
-    return arr;
-}
-int main(int argc, char** argv)
+void dfs(int index, int current)
 {
-	int test_case;
-    int T;
-    vector<int> arr;
-
-	freopen("input.txt", "r", stdin);
-
-    cin>>T;
-	for(test_case = 1; test_case <= T; ++test_case)
-	{   
-        int num, chance;
-        int tmp, idx=0;
-        int balance=0;
-        vector <int> arr;
-
-        cin>>num>>chance;
-
-        arr=makeNumArray(num);
-
-        while(chance>0){
-            int maxidx, max=0;
-
-            if(idx>=arr.size())
-                idx=idx%arr.size();
-            if(idx==max_element(arr.begin()+idx,arr.end())-arr.begin()){
-                idx++;
-            }
-
-            for(int j=arr.size()-1;j>=idx;j--){
-                    if(idx!=j&&arr[j]>max){
-                        max=arr[j];
-                        maxidx=j;
-                    }
-                
-            }
-
-            tmp=arr[idx];
-            arr[idx]=arr[maxidx];
-            arr[maxidx]=tmp;
-
-            chance--;
-            balance++;
-
-            if(balance>=arr.size())
-                idx--;
-            else
-                idx++;
-
-        }
-
-        cout<<"#"<<test_case<<" ";
-        for(int i=0;i<arr.size();i++){
-            cout<<arr[i];
-        }
-        cout<<"\n";
+    if(current == change)
+    {
+        ans = max(ans, stoi(s));
+        return;
     }
+    for(int i = index; i < s.size() - 1; i++)
+    {
+        for(int j = i + 1; j < s.size(); j++)
+        {
+            swap(s[i], s[j]);
+            dfs(i, current + 1);
+            swap(s[i], s[j]);
+        }
+    }
+}
 
-	return 0;
+int main(void)
+{
+    int t;
+
+    freopen("input.txt", "r", stdin);
+
+    cin >> t;
+    
+    for(int i = 1; i <= t; i++)
+    {
+        cin >> s >> change;
+        ans = 0;
+        
+        if(change > s.size())
+            change = s.size() - 1;
+        dfs(0, 0);
+        cout << "#" << i << " " << ans << endl;
+    }
+    cout<<stoi("123")<<endl;
+    return 0;
 }
