fac3fe1874cea0476c125c3e14746b799e978cd1
dyeonss <dyeon7310@gmail.com>
2022-08-01 14:17:04 +0900

    [D2] Title: 파리 퇴치, Time: 149 ms, Memory: 23,876 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,42 +1,37 @@
-#include <iostream>
+import java.util.Scanner;
 
-using namespace std;
-int map[15][15];
-int main(void)
-{
-    int t;
-    
-    cin >> t;
-    
-    for(int test_case = 1; test_case<= t; test_case++)
-    {
-        int n,m;
+class Solution {
+    static int[][] map = new int[15][15] ;
 
-        cin>>n>>m;
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cin>>map[i][j];
-            }
-        }
-
-        int r=0, c=0, sum=0, max=0;
-
-        for(int i=0;i<=n-m;i++){
-            for(int j=0;j<=n-m;j++){
-                sum=0;
-                for(int k=0;k<m;k++){
-                    for(int l=0;l<m;l++){
-                        sum+=map[i+k][j+l];
-                    }
-                if(sum>max)
-                    max=sum;
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int n=sc.nextInt();
+            int m= sc.nextInt();
+            int r=0, c=0, sum=0, max=0;
+            
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    map[i][j]= sc.nextInt();
                 }
             }
+            
+            for(int i=0;i<=n-m;i++){
+                for(int j=0;j<=n-m;j++){
+                    sum=0;
+                    for(int k=0;k<m;k++){
+                        for(int l=0;l<m;l++){
+                            sum+=map[i+k][j+l];
+                        }
+                        if(sum>max)
+                            max=sum;
+                    }
+                }
+            }
+            System.out.println("#"+test_case+" "+max);
         }
-
-        cout<<"#"<<test_case<<" "<<max<<"\n";
     }
-    return 0;
-
 }
