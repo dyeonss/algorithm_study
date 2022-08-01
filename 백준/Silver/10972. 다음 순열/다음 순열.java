bf84e98b6f3e0a96d04438b74988a2780dd59c03
dyeonss <dyeon7310@gmail.com>
2022-08-01 13:53:54 +0900

    [Silver III] Title: 다음 순열, Time: 392 ms, Memory: 29116 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		int [] p=new int[n];
+		StringBuilder res = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			p[i]=sc.nextInt();
+		}
+		boolean result=np(n-1, p);
+		
+		if(result==false) 
+			System.out.println("-1");
+		else {
+			for (int i = 0; i < p.length; i++) {
+				res.append(p[i]);
+				res.append(" ");
+			}
+			System.out.println(res);
+		}
+
+	}
+
+	private static boolean np(int size, int [] p) {
+		int i = size;
+
+		while (i > 0 && p[i - 1] > p[i])
+			i--;
+
+		if (i == 0)
+			return false;
+		
+		int j=size;
+		
+		while(p[i-1]>p[j])
+			j--;
+		
+		int temp=p[i-1];
+		p[i-1]=p[j];
+		p[j]=temp;
+		
+		int k=size;
+		
+		while(i<k) {
+			temp=p[i];
+			p[i]=p[k];
+			p[k]=temp;
+			i++;
+			k--;
+		}
+		
+		return true;
+	}
+
+}
