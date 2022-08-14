8e9567c2679a6c3d0d26c4d5c2d73f6e1fdd5200
dyeonss <dyeon7310@gmail.com>
2022-08-14 23:14:25 +0900

    [Bronze II] Title: 클레어와 팰린드롬, Time: 136 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/17502. 클레어와 팰린드롬/클레어와 팰린드롬.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int N=Integer.parseInt(br.readLine());
+        StringBuffer sb=new StringBuffer();
+        String s = br.readLine();
+        sb.append(s);
+        if(s.length()%2!=0&&s.charAt(s.length()/2)=='?'){
+            s=sb.replace(s.length()/2,s.length()/2+1,"a").toString();
+        }
+
+        int last=s.length()-1;
+        for (int i = 0; i < s.length()/2; i++) {
+            if (s.charAt(i)=='?'&&s.charAt(last-i)=='?'){
+                sb.replace(i,i+1,"a");
+                sb.replace(last-i,last-i+1,"a");
+            }
+            else if (s.charAt(i)=='?'){
+                sb.replace(i,i+1,s.substring(last-i,last-i+1));
+            }
+            else if (s.charAt(last-i)=='?'){
+                sb.replace(last-i,last-i+1,s.substring(i,i+1));
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
