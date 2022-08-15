d6616eedf5706e26c157e753c31326b8e620b5e3
dyeonss <dyeon7310@gmail.com>
2022-08-16 01:09:05 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 18604 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -6,44 +6,45 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N,K;
-	static int min=Integer.MAX_VALUE;
-	static int visited[];
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N=Integer.parseInt(st.nextToken());
-		K=Integer.parseInt(st.nextToken());
-		visited=new int[100001];
-		
-		find(N);
-		System.out.println(min);
-	}
-	private static void find(int n) {
-		Queue<Integer> q = new LinkedList<>();
-		int second=1;
-		
-		q.offer(n);
-		visited[n]=second;
-		
-		while(!q.isEmpty()) {
-			int loc = q.poll();
-			if(loc==K) {
-				min=visited[loc]-1;
-			}
-			second++;
-			if(loc-1>=0&&loc-1<=100000&&visited[loc-1]==0) {
-				q.offer(loc-1);
-				visited[loc-1]=visited[loc]+1;
-			}
-			if(loc+1>=0&&loc+1<=100000&&visited[loc+1]==0) {
-				q.offer(loc+1);
-				visited[loc+1]=visited[loc]+1;
-			}
-			if(loc*2>=0&&loc*2<=100000&&visited[loc*2]==0) {
-				q.offer(loc*2);
-				visited[loc*2]=visited[loc]+1;
-			}
-		}
-	}
+    static int result;
+    static boolean [] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int N=Integer.parseInt(st.nextToken());
+        int K=Integer.parseInt(st.nextToken());
+        visited=new boolean[100001];
+        bfs(N,K);
+        System.out.println(result);
+    }
+
+    private static void bfs(int n, int k) {
+        Queue<Integer> q=new LinkedList<>();
+
+        q.offer(n);
+        visited[n]=true;
+
+        while (!q.isEmpty()){
+            int len=q.size();
+
+            for (int i = 0; i < len; i++) {
+                int cur = q.poll();
+                if(cur==k)  return;
+
+                if(cur-1>=0&&cur-1<=100000&&!visited[cur-1]){
+                    q.offer(cur-1);
+                    visited[cur-1]=true;
+                }
+                if(cur+1>=0&&cur+1<=100000&&!visited[cur+1]){
+                    q.offer(cur+1);
+                    visited[cur+1]=true;
+                }
+                if(cur*2>=0&&cur*2<=100000&&!visited[cur*2]){
+                    q.offer(cur*2);
+                    visited[cur*2]=true;
+                }
+            }
+            result++;
+        }
+    }
 }
