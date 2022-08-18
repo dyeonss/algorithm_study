035dada8210ef10f9e0911c301b87fc708519bf5
dyeonss <dyeon7310@gmail.com>
2022-08-18 14:55:47 +0900

    [Gold II] Title: 빵집, Time: 428 ms, Memory: 39480 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int R,C;
+	static char [][] map;
+	static boolean [][] visited;
+	static int res;
+	static int [] dr= {-1,0,1};
+	static int [] dc= {1,1,1};
+	static boolean stop;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		R=Integer.parseInt(st.nextToken());
+		C=Integer.parseInt(st.nextToken());
+		
+		map=new char[R][C];
+		visited=new boolean[R][C];
+		
+		for (int i = 0; i < R; i++) {
+			String s=br.readLine();
+			for (int j = 0; j < C; j++) {
+				map[i][j]=s.charAt(j);
+			}
+		}
+		
+		for (int i = 0; i < R; i++) {
+			stop=false;
+			if(!visited[i][0])
+				dfs(i,0);
+		}
+		System.out.println(res);
+	}
+
+	private static void dfs(int r, int c) {
+		if(c==C-1) {
+			res++;
+			stop=true;
+			return;
+		}
+		
+		for (int i = 0; i < 3; i++) {
+			if(stop)	return;
+			int nr=r+dr[i];
+			int nc=c+dc[i];
+			
+			if(check(nr,nc)&&!visited[nr][nc]&&map[nr][nc]=='.') {
+				visited[nr][nc]=true;
+				dfs(nr,nc);
+			}
+		}
+	}
+
+	private static boolean check(int r, int c) {
+		return (r>=0&&r<R&&c>=0&&c<C);
+	}
+
+}
