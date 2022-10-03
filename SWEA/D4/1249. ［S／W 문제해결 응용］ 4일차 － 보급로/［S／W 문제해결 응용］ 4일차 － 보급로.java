87b475bff5679c8330ac3ec4f71de777ade7e036
dyeonss <dyeon7310@gmail.com>
2022-10-03 12:40:18 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 196 ms, Memory: 38,728 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class Solution {
     static int N,res;
-    static int [][] map=new int [100][100];
+    static int [][] map;
     static boolean [][] visited;
+    static int [][] total;
     static class  Pos {
         int r, c;
         int time;
@@ -16,6 +18,7 @@ public class Solution {
             this.time=time;
         }
     }
+    static PriorityQueue<Pos> pq = new PriorityQueue<>((p1,p2)->Integer.compare(p1.time,p2.time));
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -24,12 +27,16 @@ public class Solution {
         for (int tc = 1; tc <= T; tc++) {
             N=Integer.parseInt(br.readLine());
             res=Integer.MAX_VALUE;
-            visited=new boolean[100][100];
+            visited=new boolean[N][N];
+            map=new int [N][N];
+            total=new int [N][N];
+            for (int i = 0; i < N; i++)
+                Arrays.fill(total[i],Integer.MAX_VALUE);
+
             for (int i = 0; i < N; i++) {
                 String input=br.readLine();
-                for (int j = 0; j < N; j++) {
+                for (int j = 0; j < N; j++)
                     map[i][j]=input.charAt(j)-'0';
-                }
             }
             findGoal();
             sb.append("#").append(tc).append(" ").append(res).append("\n");
@@ -39,9 +46,10 @@ public class Solution {
     static int [] dr = {0, 0, -1, 1};
     static int [] dc = {-1, 1, 0, 0};
     private static void findGoal() {
-        PriorityQueue<Pos> pq = new PriorityQueue<>((p1,p2)->Integer.compare(p1.time,p2.time));
+        pq.clear();
         pq.offer(new Pos(0,0,0));
         visited[0][0]=true;
+
         while (!pq.isEmpty()){
             Pos p = pq.poll();
 
@@ -53,9 +61,10 @@ public class Solution {
             for (int i = 0; i < 4; i++) {
                 int nr=p.r+dr[i];
                 int nc=p.c+dc[i];
-                if(check(nr,nc)&&(!visited[nr][nc])){
-                    pq.offer(new Pos(nr,nc,p.time+map[nr][nc]));
+                if(check(nr,nc)&&(!visited[nr][nc])&&(p.time+map[nr][nc]<total[nr][nc])){
                     visited[nr][nc]=true;
+                    total[nr][nc]=p.time+map[nr][nc];
+                    pq.offer(new Pos(nr,nc,total[nr][nc]));
                 }
             }
         }
