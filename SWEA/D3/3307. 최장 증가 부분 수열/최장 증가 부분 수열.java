5481481fdea8bcecd4eaec30453fda9bf5075cb8
dyeonss <dyeon7310@gmail.com>
2022-10-06 13:05:44 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 171 ms, Memory: 40,056 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -1,34 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Solution {
     static int N;
     static int[] arr;
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
         for (int tc = 1; tc <= T; tc++) {
-            N = sc.nextInt();
+            N = Integer.parseInt(br.readLine());
             arr = new int[N];
-            for (int i = 0; i < N; i++) 
-                arr[i] = sc.nextInt();
-            
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
             int[] dp = new int[N];
             int max = 1;
-
             for (int i = 0; i < N; i++) {
                 dp[i] = 1;
                 for (int j = 0; j < i; j++) {
                     if (arr[j] < arr[i]) {
-                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                        if(dp[j]+1>dp[i])   dp[i]=dp[j]+1;
                     }
                 }
-                max = Math.max(max, dp[i]);
+                if(dp[i]>max)   max=dp[i];
             }
             System.out.println("#"+tc+" "+max);
         }
-        sc.close();
     }
 }
