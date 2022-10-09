f1b19e723a0c83be9a639cee7c837c6a4b9457ce
dyeonss <dyeon7310@gmail.com>
2022-10-09 18:01:45 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 195 ms, Memory: 40,676 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -4,21 +4,16 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
-public class Solution {
-    static int N;
+public class Solution{
+    static int N,res;
     static int [][] map;
-    static int [][] total;
-    static class  Pos implements Comparable<Pos>{
+    static class  Pos {
         int r, c;
         int time;
         public Pos(int r, int c,int time) {
             this.r = r;
             this.c = c;
             this.time=time;
-        }
-        @Override
-        public int compareTo(Pos o) {
-            return Integer.compare(this.time,o.time);
         }
     }
     public static void main(String[] args) throws IOException {
@@ -29,39 +24,38 @@ public class Solution {
         for (int tc = 1; tc <= T; tc++) {
             N=Integer.parseInt(br.readLine());
             map=new int [N][N];
-            total=new int [N][N];
-            for (int i = 0; i < N; i++)
-                Arrays.fill(total[i],Integer.MAX_VALUE);
-
+            res=Integer.MAX_VALUE;
             for (int i = 0; i < N; i++) {
                 String input=br.readLine();
                 for (int j = 0; j < N; j++)
                     map[i][j]=input.charAt(j)-'0';
             }
             findGoal();
-            sb.append("#").append(tc).append(" ").append(total[N-1][N-1]).append("\n");
+            sb.append("#").append(tc).append(" ").append(res).append("\n");
         }
         System.out.println(sb.toString());
     }
     static int [] dr = {0, 0, -1, 1};
     static int [] dc = {-1, 1, 0, 0};
     private static void findGoal() {
-        PriorityQueue<Pos> pq = new PriorityQueue<>();
+        boolean [][] visited = new boolean[N][N];
+        PriorityQueue<Pos> pq = new PriorityQueue<>((p1,p2)->p1.time-p2.time);
         pq.offer(new Pos(0,0,0));
-
-        total[0][0]=0;
+        visited[0][0]=true;
 
         while (!pq.isEmpty()){
             Pos p = pq.poll();
 
-            if(p.time>=total[N-1][N-1]) continue;
-
+            if(p.time>=res) continue;
+            if(p.r==N-1&&p.c==N-1)  {
+                res=Math.min(res,p.time);
+            }
             for (int i = 0; i < 4; i++) {
                 int nr=p.r+dr[i];
                 int nc=p.c+dc[i];
-                if(check(nr,nc)&&(p.time+map[nr][nc]<total[nr][nc])){
-                    total[nr][nc]=p.time+map[nr][nc];
-                    pq.offer(new Pos(nr,nc,total[nr][nc]));
+                if(check(nr,nc)&&!visited[nr][nc]){
+                    pq.offer(new Pos(nr,nc,p.time+map[nr][nc]));
+                    visited[nr][nc]=true;
                 }
             }
         }
