328de82a148de895bcf4cb1e8e140b8ed273fe07
dyeonss <dyeon7310@gmail.com>
2022-10-09 23:13:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 4,615 ms, Memory: 116,200 KB -BaekjoonHub

---
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution{
+    static int D,W,K,res;
+    static int [][] film;
+    static boolean [] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T=Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            res=Integer.MAX_VALUE;
+            D=Integer.parseInt(st.nextToken());
+            W=Integer.parseInt(st.nextToken());
+            K=Integer.parseInt(st.nextToken());
+            film=new int[D][W];
+            visited=new boolean[D];
+
+            for (int i = 0; i < D; i++) {
+                st = new StringTokenizer(br.readLine()," ");
+                for (int j = 0; j < W; j++) {
+                    film[i][j]=Integer.parseInt(st.nextToken());    //0->A, 1->B
+                }
+            }
+            test(0,0, 0,film);
+            sb.append("#").append(tc).append(" ").append(res).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static void test(int cnt,int paint, int idx,int [][] film) {
+        int [][] copy=new int[D][W];
+        if(cnt>=res){
+            return;
+        }
+        if(pass(film)){
+            res= Math.min(res,cnt);
+            return;
+        }
+        for (int i = idx; i < D; i++) {
+            if(!visited[i]){
+                visited[i]=true;
+                copy(film,copy);
+                change(copy,i,0);
+                test(cnt+1,0,i+1,copy);
+                change(copy,i,1);
+                test(cnt+1,1,i+1,copy);
+                visited[i]=false;
+            }
+        }
+    }
+
+    private static void copy(int[][] film, int[][] copy) {
+        for (int r = 0; r < D; r++) {
+            for (int c = 0; c < W; c++) {
+                copy[r][c]=film[r][c];
+            }
+        }
+    }
+
+    private static void change(int[][] copy,int i,int paint) {
+        for (int j = 0; j < W; j++) {
+            copy[i][j]=paint;
+        }
+    }
+
+    private static boolean pass(int [][] copy) {
+        boolean p=true;
+        for (int i = 0; i < W; i++) {
+            int d=copy[0][i];
+            int count=1;
+            for (int j = 1; j < D; j++) {
+                if(d!=copy[j][i]) {
+                    d = copy[j][i];
+                    count=1;
+                }
+                else count++;
+                if(count==K)    break;
+            }
+            if(count!=K){
+                p=false;
+                return false;
+            }
+        }
+        return true;
+    }
+}
