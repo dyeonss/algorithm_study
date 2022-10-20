3cccc7dff52705e71f90cfee9fd98157b6dedcb5
dyeonss <dyeon7310@gmail.com>
2022-10-20 17:24:53 +0900

    [level 2] Title: 가장 큰 수, Time: 4.19 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        int divide = 0;
+        Arrays.sort(numbers);
+        Integer[] sorted = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+
+        Arrays.sort(sorted, new Comparator<Integer>(){
+            @Override               
+            public int compare(Integer a, Integer b){
+                String atos = String.valueOf(a);
+                String btos = String.valueOf(b);
+                int alen=atos.length();
+                int blen=btos.length();
+                int max = Math.max(alen,blen);
+                
+                for(int i=0;i<max;i++){
+                    int aidx = (i>=alen?i%alen:i);
+                    int bidx = (i>=blen?i%blen:i);
+                    if(atos.charAt(aidx)!=btos.charAt(bidx))
+                        return atos.charAt(aidx)>btos.charAt(bidx)?-1:1;
+                }
+                return atos.charAt(alen-1)>btos.charAt(blen-1)?-1:1;
+            }
+        });
+        
+        for(int num:sorted)
+            answer+=String.valueOf(num);
+
+        if(answer.charAt(0)=='0')    answer="0";
+        
+        return answer;
+    }
+}
