1b3dff88565335ae23708f9c49aa7a86eb584c7f
dyeonss <dyeon7310@gmail.com>
2022-10-31 22:59:59 +0900

    [level 2] Title: 전화번호 목록, Time: 324.93 ms, Memory: 96.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        for(int i=0;i<phone_book.length-1;i++){
+            if(phone_book[i].charAt(0)!=phone_book[i+1].charAt(0))    continue;
+            if(phone_book[i].length()<phone_book[i+1].length()){
+                String sb=phone_book[i+1].substring(0,phone_book[i].length());
+                if(sb.equals(phone_book[i])){
+                    return false;
+                }   
+            }
+            else{
+                String sb=phone_book[i].substring(0,phone_book[i+1].length());
+                if(sb.equals(phone_book[i+1])){
+                    return false;
+                } 
+            }
+            
+        }
+        return answer;
+    }
+}
