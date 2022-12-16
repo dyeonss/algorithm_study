39bf549c54032c493c729ea1a6b6cb5d2ebc8731
dyeonss <dyeon7310@gmail.com>
2022-12-16 17:43:42 +0900

    [level 1] Title: 최소직사각형, Time: 8.18 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -1,31 +1,56 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-        int max=Integer.MIN_VALUE;
-        int maxidx = 0;
-       
-        for(int i=0;i<sizes.length;i++){
-            if(sizes[i][0]>max||sizes[i][1]>max){
-                maxidx=i;
-                max=sizes[i][0]>max?sizes[i][0]:sizes[i][1];
+        int maxlen = Integer.MIN_VALUE;
+        int d=0;
+        boolean flag = false;
+        int [] h = new int[sizes.length];
+        int [] v = new int[sizes.length];
+        int i=0;
+        for(int [] tmp:sizes){
+            h[i]=tmp[0];
+            v[i]=tmp[1];
+            if(tmp[0]>maxlen){  
+                maxlen=tmp[0];
+                flag=false;
+            }
+            else if(tmp[1]>maxlen){
+                maxlen=tmp[1];
+                flag=true;
+            }
+            i++;
+        }
+        if(!flag){  //max가 가로임
+            Arrays.sort(v); //세로 정렬
+            d=v[0];
+            while(true){
+                boolean poss=false;
+                for(int k=0;k<sizes.length;k++){
+                    poss=false;
+                    if(d>=sizes[k][0]&&maxlen>=sizes[k][1]) poss=true;
+                    else if(d>=sizes[k][1]&&maxlen>=sizes[k][0])    poss=true;
+                    if(!poss)   break;
+                }
+                if(poss)    break;
+                d++;
+            }
+        }else{
+            Arrays.sort(h); //세로 정렬
+            d=h[0];
+            while(true){
+                boolean poss=false;
+                for(int k=0;k<sizes.length;k++){
+                    poss=false;
+                    if(d>=sizes[k][0]&&maxlen>=sizes[k][1]) poss=true;
+                    else if(d>=sizes[k][1]&&maxlen>=sizes[k][0])    poss=true;
+                    if(!poss)   break;
+                }
+                if(poss)    break;
+                d++;
             }
         }
         
-        int m = (max==sizes[maxidx][0])?sizes[maxidx][1]:sizes[maxidx][0];
-        for(int i=0;i<sizes.length;i++){
-            if(i!=maxidx){
-                if(sizes[i][0]>=sizes[i][1]&&sizes[i][1]<=m) continue;
-                else if(sizes[i][0]>=sizes[i][1]&&sizes[i][1]>m) m=sizes[i][1];
-        
-                else if(sizes[i][0]<=sizes[i][1]&&sizes[i][0]<=m)    continue;
-
-                else if(sizes[i][0]<=sizes[i][1]&&sizes[i][0]>m) m=sizes[i][0];
-            }
-        }
-
-        answer=max*m;
-        return answer; 
+        return maxlen*d;
     }
 }
