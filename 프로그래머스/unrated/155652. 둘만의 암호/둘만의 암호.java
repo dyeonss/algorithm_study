bab6810456db6c83158e7ac11f5af2e95543bab5
dyeonss <dyeon7310@gmail.com>
2023-05-07 18:43:17 +0900

    [unrated] Title: 둘만의 암호, Time: 11.29 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,25 +1,26 @@
 import java.util.*;
+
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
-        
-        boolean [] isExist = new boolean[26];
-        
-        for(int i=0;i<skip.length();i++){
-            isExist[skip.charAt(i)-'a']=true;
-        }
-        
-        for(int i=0;i<s.length();i++){
-            int idx=0, plus=0;
-            while(idx<index){
-                plus++;       
 
-                if(isExist[(s.charAt(i)-'a'+plus)%26]==false)    idx++; //skip에 포함된 문자인 경우
-                
-            }
-            answer+=(char)((s.charAt(i)+plus-'a')%26+'a');
-        }
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            int j=1;
+            int cnt = index;
+            char tmp=' ';
             
+            while(cnt>0){
+                tmp = (char)((c+j-'a')%26+97);
+                if(skip.indexOf(tmp)==-1){
+                    // System.out.println("tmp: "+tmp);
+                    cnt--;
+                }
+                j++;
+            }
+            // System.out.println(tmp);
+            answer+=tmp+"";
+        }
         return answer;
     }
 }
