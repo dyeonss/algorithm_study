407b3fb3a08a91e4b6d0a33efe9c7da95315f182
dyeonss <dyeon7310@gmail.com>
2023-06-18 17:18:10 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,10 +1,9 @@
 class Solution {
     public String solution(String phone_number) {
         String answer = "";
-        char[] arr = phone_number.toCharArray();
-        for(int i=0; i<arr.length-4;i++){
-            arr[i]='*';
-        }
-        return String.valueOf(arr);
+      
+        phone_number=phone_number.replace(phone_number.substring(0,phone_number.length()-4),"*".repeat(phone_number.length()-4));
+        
+        return phone_number;
     }
 }
