e002e5d20c77fe5f4d55c65b8cdee2dc1fc9dd6f
dyeonss <dyeon7310@gmail.com>
2023-06-23 14:05:55 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 12.98 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,23 +1,14 @@
 import java.util.*;
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
         StringBuilder sb = new StringBuilder();
-        ArrayList<Integer> arr = new ArrayList<>();
+      
         for(int i=1;i<food.length;i++){
             if(food[i]==1)  continue;
-            for(int j=0;j<food[i]/2;j++){
-                arr.add(i);
+            for(int j=0;j<food[i]/2;j++)
                 sb.append(String.valueOf(i));
-            }
         }
         
-        sb.append("0");
-        Collections.sort(arr,Collections.reverseOrder());
-        for(Integer n: arr){
-            sb.append(String.valueOf(n));
-        }
-        
-        return sb.toString();
+        return sb.toString()+"0"+sb.reverse();
     }
 }
