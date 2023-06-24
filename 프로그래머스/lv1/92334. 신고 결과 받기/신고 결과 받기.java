9bf2622701bcdc31b7ce350fa9581d0429b71c66
dyeonss <dyeon7310@gmail.com>
2023-06-24 19:04:51 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.11 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int [id_list.length];
+        HashMap<String, ArrayList<String>> map = new HashMap();
+        HashMap<String, Integer> singo = new HashMap();
+        
+        for(String id: id_list){
+            map.put(id, new ArrayList<String>());
+            singo.put(id, 0);
+        }
+        
+        for(int i=0;i<report.length;i++){
+            String [] sarr = report[i].split(" ");
+            if(!map.get(sarr[0]).contains(sarr[1])){
+                map.get(sarr[0]).add(sarr[1]);
+                singo.put(sarr[1], singo.get(sarr[1])+1);
+            }
+        }
+        
+        for(String id: id_list){
+            if(singo.get(id)>=k){
+                System.out.println(id);
+                for(int j=0;j<id_list.length;j++){
+                    if(map.get(id_list[j]).indexOf(id)!=-1){
+                        answer[j]++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
