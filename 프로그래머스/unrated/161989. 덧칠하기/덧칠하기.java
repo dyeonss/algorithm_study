50dd66e40f31e1eca2738c4fdd2cf334c23a97fc
dyeonss <dyeon7310@gmail.com>
2023-06-30 15:30:41 +0900

    [unrated] Title: 덧칠하기, Time: 0.94 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -2,12 +2,10 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 1;
         int min=section[0];
-        int cnt=1;
         
         for(int i=1;i<section.length;i++){
             if(!(section[i]-min+1<=m)){
                 min=section[i];
-                cnt=1;
                 answer++;
             }  
         }
