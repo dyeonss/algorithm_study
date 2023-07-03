7bd6f8395c9b994a18e7a38a7bedfe130b5d304d
dyeonss <dyeon7310@gmail.com>
2023-07-03 16:24:17 +0900

    [unrated] Title: 대충 만든 자판, Time: 12.02 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -4,17 +4,13 @@ class Solution {
         int[] answer = new int[targets.length];
         
         for(int i=0;i<targets.length;i++){
-            int cnt=0;
             for(int j=0;j<targets[i].length();j++){
-                cnt = match(targets[i].charAt(j),keymap);
+                int cnt = match(targets[i].charAt(j),keymap);
                 if(cnt==-1){
+                    answer[i]=-1;
                     break;
                 }
                 answer[i]+=cnt;
-            }
-            if(cnt==-1){
-                answer[i]=-1;
-                continue;
             }
         }
         return answer;
