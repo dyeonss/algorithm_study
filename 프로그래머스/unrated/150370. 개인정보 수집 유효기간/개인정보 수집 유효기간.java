f01fbcf938e0e500f205a85051e73db1b4d6c093
dyeonss <dyeon7310@gmail.com>
2023-07-03 23:46:22 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 26.27 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -13,13 +13,25 @@ class Solution {
         for(int i=0;i<privacies.length;i++){
             String[] date = privacies[i].substring(0,10).split("\\.");
             String type = privacies[i].substring(11,12);
-            int year = Integer.valueOf(date[0]);
-            int month = Integer.valueOf(date[1]);
-            int day = Integer.valueOf(date[2]);
             int plus = t.get(type);
-           
-            int after = year*12*28+(month+plus)*28+day;
-            if(after<=todayD)            
+            int year = Integer.valueOf(date[0]);
+            int month = Integer.valueOf(date[1])+plus;
+            int day = Integer.valueOf(date[2]);
+                      
+            if(month>12){
+                if(month%12==0){
+                    year+=month/12-1;
+                    month=12;
+                }
+                else{
+                    year+=month/12;
+                    month%=12;
+                }
+            }
+            
+            int after = Integer.valueOf(year+(month<10?"0":"")+month+(day<10?"0":"")+day);
+         
+            if(after<=Integer.valueOf(today.replace(".","")))            
                 result.add(i+1);
         }
         
