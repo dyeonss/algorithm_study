4e94ec849fcbaae5b79beed92af50c4ed17153cf
dyeonss <dyeon7310@gmail.com>
2023-07-05 18:52:38 +0900

    [unrated] Title: 과일 장수, Time: 0.36 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Arrays.sort(score);
+
+        for(int i=1;i<=score.length/m;i++){
+            answer+=(score[score.length-m*i]*m);
+        }
+        return answer;
+    }
+}
