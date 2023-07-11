dd197b2a1d2509ea4879cde84f9da0430c12f14c
dyeonss <dyeon7310@gmail.com>
2023-07-11 23:40:30 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 3.23 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> box = new Stack<>();
+        
+        for(int i=0;i<moves.length;i++){
+            for(int j=0;j<board.length;j++){
+                if(board[j][moves[i]-1]!=0){
+                    int n=board[j][moves[i]-1];
+                    board[j][moves[i]-1]=0;
+                    if(box.size()>0){
+                        if(n==box.get(box.size()-1)){
+                            System.out.println(n);
+                            box.pop();
+                            answer+=2;
+                            break;
+                        }
+                    }
+                    box.push(n);
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
