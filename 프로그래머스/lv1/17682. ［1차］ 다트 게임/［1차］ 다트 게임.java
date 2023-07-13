e2d29388c6ada5660e356f4475995bb97525dd12
dyeonss <dyeon7310@gmail.com>
2023-07-13 17:55:17 +0900

    [level 1] Title: [1차] 다트 게임, Time: 1.07 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -3,11 +3,8 @@ class Solution {
     public int solution(String dartResult) {
         int answer = 0;
         String s="";
-        boolean isNum=false;
         int[] score=new int[3];
         int j=0;
-        ArrayList<Integer> star=new ArrayList<Integer>();
-        ArrayList<Integer> sharp=new ArrayList<Integer>();
         
         for(int i=0;i<dartResult.length();i++){
             char c = dartResult.charAt(i);
@@ -15,7 +12,6 @@ class Solution {
                 s+=dartResult.substring(i,i+1);
             }
             else if(c=='S'||c=='D'||c=='T'){
-                System.out.println(s);
                 if(c=='S')  score[j]=Integer.valueOf(s);
                 else if(c=='D') score[j]=(int)Math.pow(Integer.valueOf(s),2);
                 else score[j]=(int)Math.pow(Integer.valueOf(s),3);
@@ -31,7 +27,6 @@ class Solution {
             }
             else if(c=='#'){
                 score[j-1]=score[j-1]*-1;
-                
             }
         }
         
