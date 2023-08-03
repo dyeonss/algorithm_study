4f494fd7310d69a3b61afa716aa20f70e781c348
dyeonss <dyeon7310@gmail.com>
2023-08-04 03:28:02 +0900

    [level 2] Title: [3차] n진수 게임, Time: 3.54 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -6,15 +6,18 @@ class Solution {
         String num = Integer.toString(start,n);
         int idx = 0;
         int order = 0;
-        StringBuilder game = new StringBuilder();
+  
         while(t>0){
             String str = num.substring(idx,idx+1).toUpperCase();
+            
             if(order%m==p-1){
                 answer.append(str);
                 t--;
             }
+            
             idx++;
             order++;
+            
             if(idx==num.length()){    
                 idx=0;
                 start++;
