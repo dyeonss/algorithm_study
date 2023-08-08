29e09a7c31c9247a0adcbcba837793723c412f46
dyeonss <dyeon7310@gmail.com>
2023-08-08 15:16:00 +0900

    [level 2] Title: 더 맵게, Time: 1640.01 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -2,26 +2,20 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        boolean check = false;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
         for(int s:scoville) pq.offer(s);
         
-        while(!pq.isEmpty()){
-            int a = pq.poll();
-            if(a>=K){
-                check=true;
-                break;
-            }
-            if(pq.size()==0)    break;
-            int b = pq.poll();
-            
-            int r = a+2*b;
-            pq.offer(r);
+        while(pq.size()>=2){
+            int least = pq.poll();
+            if(least>=K)    break;
+            int second = pq.poll();
+
+            pq.offer(least+2*second);
             answer++;        
         }
         
-        if(!check)  answer = -1;
+        if(pq.peek()<K) answer = -1;
         return answer;
     }
 }
