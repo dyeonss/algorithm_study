a940e3634640e957f2faabb2c57b19d45e4d6d1e
dyeonss <dyeon7310@gmail.com>
2023-08-10 14:38:48 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.64 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,7 +1,6 @@
 import java.util.*;
-import java.text.*;
 class Solution {
-    public int[] solution(int[] fees, String[] records) throws ParseException{
+    public int[] solution(int[] fees, String[] records) {
         int[] answer = {};
         int basicTime = fees[0];
         int basicFee = fees[1];
@@ -9,28 +8,31 @@ class Solution {
         int unitFee = fees[3];
         TreeMap<String, String> map = new TreeMap<>();
         TreeMap<String, Integer> time = new TreeMap<>();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-        
+       
         for(String r: records){
             String [] arr = r.split(" ");
             if(map.get(arr[1])==null){   //입차 시
                 map.put(arr[1], arr[0]);
             }
             else{   //출차 시
-                Date in = sdf.parse(map.get(arr[1])); //입차시간
-                Date out = sdf.parse(arr[0]);  //출차시간
-                int diff = (int)(out.getTime() - in.getTime())/(1000*60); 
+                int hour = Integer.valueOf(map.get(arr[1]).substring(0,2));
+                int minute = Integer.valueOf(map.get(arr[1]).substring(3,5));
+                int in = hour*60+minute;
+                hour = Integer.valueOf(arr[0].substring(0,2));
+                minute = Integer.valueOf(arr[0].substring(3,5));
+                int out = hour*60+minute;
                 map.remove(arr[1]);
-                time.put(arr[1],time.getOrDefault(arr[1],0)+diff);
+                time.put(arr[1],time.getOrDefault(arr[1],0)+(out-in));
             }
         }
         
         if(map.size()>0){
             for(String m: map.keySet()){
-                Date in = sdf.parse(map.get(m)); //입차시간
-                Date out = sdf.parse("23:59");  //출차시간
-                int diff = (int)(out.getTime() - in.getTime())/(1000*60); 
-                time.put(m,time.getOrDefault(m, 0)+diff);
+                int hour = Integer.valueOf(map.get(m).substring(0,2));
+                int minute = Integer.valueOf(map.get(m).substring(3,5));
+                int in = hour*60+minute;
+                int out = 23*60+59;
+                time.put(m,time.getOrDefault(m, 0)+(out-in));
             }
         }
         
