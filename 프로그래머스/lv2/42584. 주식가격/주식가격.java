d2a7db6bba860747202069b46148952757567465
dyeonss <dyeon7310@gmail.com>
2023-08-12 19:26:39 +0900

    [level 2] Title: 주식가격, Time: 8.37 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -2,19 +2,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < prices.length; i++) {
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                answer[stack.peek()] = i - stack.peek();
-                stack.pop();  // 답을 구했기 때문에 stack에서 제거
+        int minValue = 10001;
+        
+        for(int i=prices.length-1; i>=0; i--){
+            if(prices[i] <= minValue){
+                minValue = prices[i];
+                answer[i] = prices.length - i - 1;
             }
-            stack.push(i);
-        }
-
-        while (!stack.isEmpty()) { // 값을 구하지 못한 index == 끝까지 가격이 떨어지지 않은 주식
-            answer[stack.peek()] = prices.length - stack.peek() - 1;
-            stack.pop();
+            else{
+                int idx = i;
+                for(int j = i+1;j < prices.length;j++){
+                    if(prices[i] > prices[j]){
+                        idx = j;
+                        break;
+                    }
+                }
+                answer[i] = idx - i;
+            }
         }
 
         return answer;
