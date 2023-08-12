43ec420148119e3b567552e643d46e472fcca467
dyeonss <dyeon7310@gmail.com>
2023-08-12 18:58:50 +0900

    [level 2] Title: 주식가격, Time: 23.17 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -2,13 +2,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
         
-        for(int i=0;i<prices.length;i++){
-            for(int j=i+1;j<prices.length;j++){
-                answer[i]++;
-                if(prices[j]<prices[i])  break;
+        for(int i=prices.length-1;i>=0;i--) stack.push(prices[i]);
+        
+        int j=1;
+        while(!stack.isEmpty()){
+            int n = stack.pop();
+            int cnt = 0;
+            for(int i=j;i<prices.length;i++){
+                cnt++;
+                if(prices[i]<n) break;
             }
+            answer[j-1]=cnt;
+            j++;
         }
+        
+        
         return answer;
     }
 }
