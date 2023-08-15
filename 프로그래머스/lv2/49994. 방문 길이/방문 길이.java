ca4c2e52fbcf07813981701ef9c3b06fc57b9530
dyeonss <dyeon7310@gmail.com>
2023-08-16 00:43:10 +0900

    [level 2] Title: 방문 길이, Time: 19.23 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
@@ -1,87 +1,23 @@
 import java.util.*;
 class Solution {
-    public static class Pos{
-        int r,c;
-        public Pos(int r, int c){
-            this.r = r;
-            this.c = c;
-        }
-    }
     public int solution(String dirs) {
-        int answer = 0;
-        HashMap<String, ArrayList<Pos>> map = new HashMap<>();   //도착, 출발
-        char[] arr = dirs.toCharArray();
-        int r=0,c=0;
-        for(char ch:arr){
-            int nr = r;
-            int nc = c;
-            if(ch=='U')
-                nr-=1;
-            else if(ch=='D')
-                nr+=1;
-            else if(ch=='R')
-                nc+=1;
-            else
-                nc-=1;
+        char[] dir_list = dirs.toCharArray();
+        int r=0, c=0;
+        HashSet<String> set = new HashSet<>();
+        
+        for(char dir: dir_list){
+            int nr = r, nc = c;
+            if(dir == 'U')  nr-=1;
+            else if(dir == 'D') nr+=1;
+            else if(dir == 'R') nc+=1;
+            else    nc-=1;
             
             if(nr<-5||nr>5||nc<-5||nc>5)    continue;
-            
-            if(map.containsKey(nr+" "+nc)){
-                ArrayList<Pos> list = map.get(nr+" "+nc);
-                boolean check = true;
-                for(Pos p:list){
-                    if(p.r==r&&p.c==c){
-                        check = false;
-                        break;
-                    }
-                }
-                //처음 가는 길
-                if(check){
-                    list.add(new Pos(r,c));
-                    map.put(nr+" "+nc,list);
-                    list = new ArrayList<>();
-                    if(map.get(r+" "+c)!=null){
-                        list = map.get(r+" "+c);
-                    }
-                    list.add(new Pos(nr,nc));
-                    map.put(r+" "+c,list);
-                    answer++;
-                }
-            }
-            else if(map.containsKey(r+" "+c)){
-                ArrayList<Pos> list = map.get(r+" "+c);
-                boolean check = true;
-                for(Pos p:list){
-                    if(p.r==nr&&p.c==nc){
-                        check = false;
-                        break;
-                    }
-                }
-                //처음 가는 길
-                if(check){
-                    list.add(new Pos(nr,nc));
-                    map.put(r+" "+c,list);
-                    list = new ArrayList<>();
-                    if(map.get(nr+" "+nc)!=null){
-                        list = map.get(nr+" "+nc);
-                    }
-                    list.add(new Pos(r,c));
-                    map.put(nr+" "+nc,list);
-                    answer++;
-                }
-            }
-            else{   
-                ArrayList<Pos> list = new ArrayList<>();
-                list.add(new Pos(r,c));
-                map.put(nr+" "+nc,list);
-                ArrayList<Pos> rev = new ArrayList<>();
-                list.add(new Pos(nr,nc));
-                map.put(r+" "+c,list);
-                answer++;
-            }
-            r=nr;
-            c=nc; 
+            set.add(nr+""+nc+" "+r+""+c);
+            set.add(r+""+c+" "+nr+""+nc);
+            r = nr;
+            c = nc; 
         }
-        return answer;
+        return set.size()/2;
     }
 }
