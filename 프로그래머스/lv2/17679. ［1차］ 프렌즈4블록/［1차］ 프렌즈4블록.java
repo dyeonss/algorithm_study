60db60e136317bac814b63a997be431aa4b8ae56
dyeonss <dyeon7310@gmail.com>
2023-08-22 23:26:42 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 1.74 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -0,0 +1,74 @@
+import java.util.*;
+class Solution {
+    public int[] dr = {0,1,0};   //시계방향
+    public int[] dc = {1,0,-1};
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        int size = board.length;
+        char[][] game = new char[n][m];
+        
+        for(int i=0;i<m;i++)    game[i]=board[i].toCharArray();
+        
+        ArrayList<int[]> loc = new ArrayList<>();
+        
+        while(true){
+            loc.clear(); 
+            for(int i=0;i<m-1;i++){
+                for(int j=0;j<n-1;j++){
+                    if(game[i][j]!=' '){
+                        char ch = game[i][j];
+                        boolean check = true;
+                        int nr = i;
+                        int nc = j;
+                        for(int k=0;k<3;k++){
+                            nr += dr[k];
+                            nc += dc[k];
+                            if(ch!=game[nr][nc]){
+                                check = false;
+                                break;
+                            }    
+                        }
+                        if(check){
+                            loc.add(new int[]{i,j});  
+                            nr = i;
+                            nc = j;
+                            for(int k=0;k<3;k++){
+                                nr += dr[k];
+                                nc += dc[k];
+                                loc.add(new int[]{nr,nc});  
+                            }
+                        }
+                    }
+                }
+            }
+        
+            if(loc.size()==0)   break;
+            
+            for(int i=0;i<loc.size();i++){
+                int[] coor = loc.get(i);
+                // System.out.println(coor[0]+" "+coor[1]);
+                if(game[coor[0]][coor[1]]!=' '){
+                    game[coor[0]][coor[1]]=' ';
+                    answer++;
+                }  
+            }
+            
+            //밑으로 내려주기
+            for(int i=n-1;i>=0;i--){
+                for(int j=0;j<m;j++){
+                    //칸이 비어있으면 그 열의 밑에서부터 맨위까지 조사해서 내리기
+                    if(game[i][j]==' '){
+                        for(int k=i-1;k>=0;k--){
+                            if(game[k][j]!=' '){
+                                game[i][j]=game[k][j];
+                                game[k][j]=' ';
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
