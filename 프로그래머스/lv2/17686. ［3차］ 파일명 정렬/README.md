633e490cfbef90adf08b19a3bf20e6d58552bda9
dyeonss <dyeon7310@gmail.com>
2023-08-22 13:59:50 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 58.63 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/README.md
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 파일명 정렬 - 17686 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686#) 
 
 ### 성능 요약
 
-메모리: 81.7 MB, 시간: 43.72 ms
+메모리: 123 MB, 시간: 58.63 ms
 
 ### 구분
 
--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,46 +1,28 @@
 import java.util.*;
 class Solution {
-    public static class Program implements Comparable<Program>{
-        String head;
-        int number;
-        String name;
-        int order;
-        public Program(String head, int number, String name, int order){
-            this.head = head;
-            this.number = number;
-            this.name = name;
-            this.order = order;
-        }
-        @Override
-        public int compareTo(Program o){
-            if(this.head.equals(o.head)){
-                if(this.number == o.number) return this.order - o.order;
-                return this.number - o.number;
-            }   
-            return this.head.compareTo(o.head);
-        }
-    }
     public String[] solution(String[] files) {
         String[] answer = new String[files.length];
-        int i = 0;
-        ArrayList<String> list = new ArrayList<>();
-        PriorityQueue<Program> pq = new PriorityQueue<>();
         
-        for(String f:files){
-            String head = f.split("[0-9]")[0].toLowerCase();
-            int len = head.length();
-            String version = f.substring(head.length()).split("[^0-9]")[0];
-            if(version.length()>5)    version = version.substring(0,5);
-            int v = Integer.valueOf(version);
-            System.out.println(head+" "+v);
-            Program p = new Program(head,v,f,i++);
-            pq.offer(p);
-        }
-        i=0;
-        while(!pq.isEmpty()){
-            Program p = pq.poll();
-            answer[i++]=p.name;
-        }
+        Arrays.sort(files, new Comparator<String>(){
+            @Override
+            public int compare(String s1, String s2){
+                String h1 = s1.split("[0-9]")[0].toLowerCase();
+                String h2 = s2.split("[0-9]")[0].toLowerCase();
+                String v1 = s1.substring(h1.length()).split("[^0-9]")[0];
+                String v2 = s2.substring(h2.length()).split("[^0-9]")[0];
+                if(v1.length()>5)    v1 = v1.substring(0,5);
+                if(v2.length()>5)    v2 = v2.substring(0,5);
+                int n1 = Integer.valueOf(v1);
+                int n2 = Integer.valueOf(v2);
+                if(h1.equals(h2))
+                    return Integer.compare(n1,n2);
+                else
+                    return h1.compareTo(h2);
+            }
+        });
+        
+        for(int i=0;i<files.length;i++) answer[i]=files[i];
+        
         return answer;
     }
 }
