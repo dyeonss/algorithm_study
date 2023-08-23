f262fda066b650a59e715d61e9a8ad6a5009db86
dyeonss <dyeon7310@gmail.com>
2023-08-23 16:33:47 +0900

    [unrated] Title: 롤케이크 자르기, Time: 303.99 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -2,30 +2,20 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        Set<Integer> all = new HashSet<>();
         HashMap<Integer, Integer> roll1 = new HashMap<>();
         HashMap<Integer, Integer> roll2 = new HashMap<>();
-        int idx = 0;
-        
-        for(int t: topping) all.add(t);
-        
-        for(int t:topping){
-            if(roll1.size()==all.size()/2)  break;
+
+        for(int t:topping)
             roll1.put(t,roll1.getOrDefault(t,0)+1);
-            idx++;
-        }
         
-        for(int i=idx;i<topping.length;i++)
-            roll2.put(topping[i],roll2.getOrDefault(topping[i],0)+1);
-        
-        
-        for(int i=idx;i<topping.length;i++){
+        for(int i=topping.length-1;i>=0;i--){
             if(roll1.size()==roll2.size())  answer++;
             
             int t = topping[i];
-            roll1.put(t,roll1.getOrDefault(t,0)+1);
-            if(roll2.get(t)!=null) roll2.put(t,roll2.get(t)-1);
-            if(roll2.get(t)==0)  roll2.remove(t);
+            
+            roll2.put(t,roll2.getOrDefault(t,0)+1);
+            if(roll1.get(t)!=null) roll1.put(t,roll1.get(t)-1);
+            if(roll1.get(t)==0)  roll1.remove(t);
         }
         
         return answer;
