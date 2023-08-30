2668e1bfce69ae2deefccd4c719b5561ce168cfc
dyeonss <dyeon7310@gmail.com>
2023-08-30 17:32:36 +0900

    [level 2] Title: 택배상자, Time: 164.46 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.java
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -2,35 +2,19 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        LinkedList<Integer> main = new LinkedList<Integer>();
-        Stack<Integer> sub = new Stack<Integer>();
-        HashMap<Integer,Boolean> in = new HashMap<>();
-        int idx=0, n=order.length;
-       
-        for(int i=1;i<=n;i++){
-            main.offer(i);
-        }   
-   
-        while(idx<n){
-            if(!main.isEmpty()&&main.peek()==order[idx]){
-                main.poll();
-                idx++;
-                answer++;
+        int idx = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=1;i<=order.length;i++){
+            stack.push(i);
+            while(!stack.isEmpty()){
+                if(stack.peek()==order[idx]){
+                    stack.pop();
+                    idx++;
+                    answer++;
+                }
+                else    break;
             }
-            else if(sub.size()>0&&(order[idx]==sub.peek())){
-                int s=sub.peek();
-                sub.pop();
-                in.remove(s);
-                idx++;
-                answer++;
-            }
-            else if(!main.isEmpty()&&main.peek()!=order[idx]&&in.get(order[idx])==null){
-                int m=main.poll();
-                sub.push(m);
-                in.put(m, true);
-            }
-            else if(!main.isEmpty()&&main.peek()!=order[idx]&&sub.size()>0&&sub.peek()!=order[idx])
-                break;
         }
         return answer;
     }
