c107de75469a3f495960a0345b56f9c3af12de84
dyeonss <dyeon7310@gmail.com>
2023-09-06 00:42:33 +0900

    [level 2] Title: 삼각 달팽이, Time: 10.72 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,6 +1,35 @@
 class Solution {
     public int[] solution(int n) {
-        int[] answer = {};
+        int[] answer = new int[n*(n+1)/2];
+        int[][] board = new int[n][n];
+        int dir=0, r=-1,c=0;
+        int start=1,idx=0;
+        int size = n;
+        
+        while(n>0){
+            if(dir%3==0){
+                for(int i=0;i<n;i++)
+                    board[++r][c]=start++;
+            }
+            else if(dir%3==1){
+                for(int i=0;i<n;i++)
+                    board[r][++c]=start++;
+            }
+            else if(dir%3==2){
+                for(int i=0;i<n;i++)
+                    board[--r][--c]=start++;
+            }
+            dir++;
+            n--;
+            
+        }
+        
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if(board[i][j]!=0)
+                    answer[idx++]=board[i][j];
+            }
+        }
         return answer;
     }
 }
