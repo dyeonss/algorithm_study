b69aabd493831e78864d30f3a0912d7b3da4301a
dyeonss <dyeon7310@gmail.com>
2023-11-15 12:01:50 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 20.30 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -6,37 +6,30 @@ class Solution {
     public long solution(String expression) {
         String[][] prior={{"+","-","*"},{"+","*","-"},{"-","+","*"},{"-","*","+"},{"*","-","+"},{"*","+","-"}}; 
         
-        expression=expression.replace("+","/+");
-        expression=expression.replace("-","/-");
-        expression=expression.replace("*","/*");
         for(int i=0;i<6;i++){
             String str=expression;
             for(int j=0;j<3;j++){
                 String c=prior[i][j];
                 
                 while(true){
-                    Pattern p=Pattern.compile("m*[0-9]+/\\"+c+"m*[0-9]+");
+                    Pattern p=Pattern.compile("m*[0-9]+\\"+c+"m*[0-9]+");
                     Matcher m=p.matcher(str);
                     if(!m.find())   break;
                     String s=m.group();
-                    String[] arr=s.split("/\\"+c);
-                    Long result=0L,n1=0L,n2=0L;
-                    System.out.println("s: "+s);
+                    String[] arr=s.split("\\"+c);
+                    Long result=0L;
 
                     arr[0]=arr[0].replace("m","-");
                     arr[1]=arr[1].replace("m","-");
-                    System.out.println("arr: "+arr[0]+" "+arr[1]);
-                    n1=Long.valueOf(arr[0]);
-                    n2=Long.valueOf(arr[1]);
 
                     if(c.equals("+")){
-                        result=n1+n2;
+                        result=Long.valueOf(arr[0])+Long.valueOf(arr[1]);
                     }
                     else if(c.equals("-")){
-                        result=n1-n2;
+                        result=Long.valueOf(arr[0])-Long.valueOf(arr[1]);
                     }
                     else{
-                        result=n1*n2;
+                        result=Long.valueOf(arr[0])*Long.valueOf(arr[1]);
                     }
                     str=str.replace(s,result<0?"m"+String.valueOf(Math.abs(result)):String.valueOf(result));
                     }
