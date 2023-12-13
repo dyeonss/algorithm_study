55392caecb6df306a496810ec8dd586a02e4a9eb
dyeonss <dyeon7310@gmail.com>
2023-12-13 22:25:06 +0900

    [level 2] Title: 배달, Time: 3.63 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,42 +1,42 @@
 import java.util.*;
 class Solution {
+    public class Node{
+        int n,v,d;
+        public Node(int n, int v, int d){
+            this.n=n;
+            this.v=v;
+            this.d=d;
+        }
+    }
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        int[][] village = new int[N][N];
-        int[] min=new int[N];
-        Queue<int[]> q =new LinkedList<>();
+        ArrayList<ArrayList<Node>> conn=new ArrayList<>();
+        int[] dist=new int[N+1];
         
-        for(int i=1;i<N;i++)
-            min[i]=Integer.MAX_VALUE;
+        for(int i=0;i<N+1;i++)    conn.add(new ArrayList<>());
         
         for(int i=0;i<road.length;i++){
-            if(village[road[i][0]-1][road[i][1]-1]==0||(village[road[i][0]-1][road[i][1]-1]!=0&&road[i][2]<village[road[i][0]-1][road[i][1]-1])){
-                village[road[i][0]-1][road[i][1]-1]=road[i][2];
-                village[road[i][1]-1][road[i][0]-1]=road[i][2];
-            }
+            conn.get(road[i][0]).add(new Node(road[i][0],road[i][1],road[i][2]));
+            conn.get(road[i][1]).add(new Node(road[i][1],road[i][0],road[i][2]));
         }
         
-        for(int i=0;i<N;i++){
-            if(village[0][i]!=0){
-                q.offer(new int[]{i,village[0][i]});    //도착점, 비용
-                min[i]=village[0][i];
-            }   
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[1]=0;
+        
+        PriorityQueue<Node> pq=new PriorityQueue<>((o1,o2)->{return o1.d-o2.d;});
+        
+        pq.addAll(conn.get(1));
+        
+        while(!pq.isEmpty()){
+            Node n=pq.poll();
+            if(n.d+dist[n.n]>dist[n.v]) continue;
+            dist[n.v]=n.d+dist[n.n];
+            pq.addAll(conn.get(n.v));
         }
         
-        while(!q.isEmpty()){
-            int[] arr=q.poll();
-            for(int i=0;i<N;i++){
-                if(village[arr[0]][i]!=0&&(arr[1]+village[arr[0]][i])<min[i]){
-                    q.offer(new int[]{i,arr[1]+village[arr[0]][i]});
-                    min[i]=arr[1]+village[arr[0]][i];
-                } 
-            }
+        for(int i=1;i<N+1;i++){
+            if(dist[i]<=K)  answer++;
         }
-        
-        for(int i=0;i<N;i++){
-            if(min[i]<=K)   answer++;
-        }
-        
         return answer;
     }
 }
