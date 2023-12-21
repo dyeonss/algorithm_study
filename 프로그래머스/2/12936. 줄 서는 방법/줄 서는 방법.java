842051c57b077543fd512930be81c91bd0ba9a95
dyeonss <dyeon7310@gmail.com>
2023-12-22 01:05:15 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -3,22 +3,19 @@ class Solution {
     public int[] solution(int n, long k) {
         int[] answer = new int[n];
         ArrayList<Integer> list=new ArrayList<>();
-        long cnt=1;
-        int idx=0;
+        long total=1;
+        for(int i=1;i<=n;i++)   list.add(i);
+        for(int i=n;i>=1;i--)   total*=i;
         
-        for(int i=1;i<=n;i++){
-            list.add(i);
-            cnt*=i;
-        }    
         k--;
-        
-        while(n>0){
-            answer[idx++]=list.remove((int)(k/(cnt/n)));
-            k%=(cnt/n);
-            cnt/=n;
+        int nn=n;
+        for(int i=0;i<nn;i++){
+            total=total/n;
             n--;
+            answer[i]=list.get((int)(k/total));
+            list.remove((int)(k/total));
+            k=k%total;
         }
-        
         return answer;
     }
 }
