4b2089ed95f73297d9b4b3a75a70e4af9e3415ab
dyeonss <dyeon7310@gmail.com>
2023-12-28 15:10:19 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.56 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -7,20 +7,22 @@ class Solution {
         Arrays.sort(weights);
         
         for(int i=0;i<weights.length-1;i++){
+            if(i>0){
+                if(weights[i]==weights[i-1]){
+                    answer+=--cnt;
+                    continue;
+                }
+            }
+            cnt=0;
             for(int j=i+1;j<weights.length;j++){
                 if(weights[i]==weights[j]||
                   4*weights[i]==3*weights[j]||
                   4*weights[i]==2*weights[j]||
                   3*weights[i]==2*weights[j]){
-                    answer++;
                     cnt++;
                 }
             }
-            if(weights[i]==weights[i+1]){
-                answer+=cnt-1;
-                i++;
-            }
-            cnt=0;
+            answer+=cnt;
         }
         
         return answer;
