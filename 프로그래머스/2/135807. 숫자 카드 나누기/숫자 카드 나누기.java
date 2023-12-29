d10349c364946f1aa7656b0f8e8c2c50a9f25111
dyeonss <dyeon7310@gmail.com>
2023-12-29 13:54:42 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,60 +1,36 @@
-import java.util.*;
 class Solution {
-    public static ArrayList<Integer> result=new ArrayList<>();
     public int solution(int[] arrayA, int[] arrayB) {
         int answer = 0;
-        Arrays.sort(arrayA);
-        Arrays.sort(arrayB);
+        int a=arrayA[0];
+        int b=arrayB[0];
+        boolean checkA=true;
+        boolean checkB=true;
         
-        ArrayList<Integer> cdA=getCd(arrayA);
-        ArrayList<Integer> cdB=getCd(arrayB);
-
-        Collections.sort(cdA);
-        Collections.sort(cdB);
-        getResults(cdA,arrayB);
-        getResults(cdB,arrayA);
-        
-        if(result.size()==0)    return 0;
-        Collections.sort(result,Collections.reverseOrder());
-        return result.get(0);
-    }
-    public ArrayList<Integer> getCd(int[] array){
-        ArrayList<Integer> min=new ArrayList<>();
-        ArrayList<Integer> cd=new ArrayList<>();
-        
-        for(int i=1;i<=Math.sqrt(array[0]);i++){
-            if(array[0]%i==0){
-                min.add(i);
-                min.add(array[0]/i);
-            }
+        for(int i=1;i<arrayA.length;i++){
+            a=getGcd(a,arrayA[i]);
+            b=getGcd(b,arrayB[i]);
         }
         
-        for(int i=0;i<min.size();i++){
-            boolean check=true;
-            for(int j=0;j<array.length;j++){
-                if(array[j]%min.get(i)!=0){
-                    check=false;
-                    break;
-                }
-            }
-            if(check)   cd.add(min.get(i));
-        }
-        return cd;
-    }
-    public void getResults(ArrayList<Integer> cd,int[] array){
-        
-        for(int i=cd.size()-1;i>=0;i--){
-            boolean check=true;
-            for(int j=0;j<array.length;j++){
-                if(array[j]%cd.get(i)==0){
-                    check=false;
-                    break;
-                }
-            }
-            if(check){
-                result.add(cd.get(i));
+        for(int i=0;i<arrayA.length;i++){
+            if(arrayA[i]%b==0){
+                checkB=false;
                 break;
             }
         }
+        
+        for(int i=0;i<arrayB.length;i++){
+            if(arrayB[i]%a==0){
+                checkA=false;
+                break;
+            }
+        }
+        
+        if(!checkA&&!checkB)    return 0;
+        if(checkA&&checkB)  return Math.max(a,b);
+        return checkA?a:b;
+    }
+    public int getGcd(int x,int y){
+        if(y==0)    return x;
+        return getGcd(y, x%y);
     }
 }
