dd3549bc3f708d652be513b945725da9daa2441b
dyeonss <dyeon7310@gmail.com>
2024-01-02 19:35:16 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.31 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,50 +1,54 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+ 
 class Solution {
-    static int[] dr={-1,1,0,0};
-    static int[] dc={0,0,-1,1};
-    public int[] solution(String[][] places) {
-        int[] answer = new int[5];
-        
-        for(int i=0;i<places.length;i++){
-            boolean check=true;
-            for(int j=0;j<places[i].length;j++){
-                for(int k=0;k<places[i][j].length();k++){
-                    if(places[i][j].charAt(k)=='P'){
-                        if(!distance(places,i,j,k)){
-                            check=false;
-                            break;
-                        }   
+    public static int[] solution(String[][] places) {
+        int[] answer = new int[places.length];
+ 
+        for (int i = 0; i < places.length; i++) {
+            String[] p = places[i];
+ 
+            boolean isOk = true;
+            for (int r = 0; r < 5 && isOk; r++) {
+                for (int c = 0; c < 5 && isOk; c++) {
+                    if (p[r].charAt(c) == 'P') {
+                        if (!bfs(r, c, p))
+                            isOk = false;
                     }
                 }
-                if(!check)  break;
             }
-            System.out.println();
-            if(check)   answer[i]=1;
+            answer[i] = isOk ? 1 : 0;
         }
+ 
         return answer;
     }
-    public boolean distance(String[][] places,int i,int j,int k){
-        int r=j, c=k;
-        Queue<int[]> q=new LinkedList<>();
-        boolean[][] visited=new boolean[5][5];
-        q.offer(new int[]{r,c});
-        visited[r][c]=true;
-
-        while(!q.isEmpty()){
-            int[] arr=q.poll();
-
-            for(int l=0;l<4;l++){
-                int nr=arr[0]+dr[l];
-                int nc=arr[1]+dc[l];
-                if(nr<0||nr>4||nc<0||nc>4)  continue;
-                if(visited[nr][nc]||places[i][nr].charAt(nc)=='X'||(Math.abs(r-nr)+Math.abs(c-nc))>2)  continue;
-                if(places[i][nr].charAt(nc)=='P'){
+ 
+    private static boolean bfs(int r, int c, String[] p) {
+        int dr[] = { -1, 1, 0, 0 };
+        int dc[] = { 0, 0, -1, 1 };
+ 
+        Queue<int[]> queue = new LinkedList<int[]>();
+        queue.offer(new int[] { r, c });
+ 
+        while (!queue.isEmpty()) {
+            int[] position = queue.poll();
+ 
+            for (int i = 0; i < 4; i++) {
+                int nr = position[0] + dr[i];
+                int nc = position[1] + dc[i];
+ 
+                if (nr < 0 || nc < 0 || nr >= 5 || nc >= 5 || (nr == r && nc == c))
+                    continue;
+ 
+                int d = Math.abs(nr - r) + Math.abs(nc - c);
+ 
+                if (p[nr].charAt(nc) == 'P' && d <= 2)
                     return false;
-                }
-                q.offer(new int[]{nr,nc});
-                visited[nr][nc]=true;
+                else if (p[nr].charAt(nc) == 'O' && d < 2)
+                    queue.offer(new int[] { nr, nc });
             }
         }
+ 
         return true;
     }
 }
