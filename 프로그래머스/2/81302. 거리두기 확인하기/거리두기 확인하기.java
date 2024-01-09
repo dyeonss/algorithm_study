b89d7aa4b2f55ca595891f5211a691be785c752e
dyeonss <dyeon7310@gmail.com>
2024-01-09 17:37:20 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.20 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -2,47 +2,40 @@ import java.util.*;
 class Solution {
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
-
+        int[] dr={-1,1,0,0};
+        int[] dc={0,0,-1,1};
+        
         for(int i=0;i<places.length;i++){
-            boolean check=true;
+            answer[i]=1;
             for(int j=0;j<places[i].length;j++){
                 for(int k=0;k<places[i][j].length();k++){
                     if(places[i][j].charAt(k)=='P'){
-                        if(!distance(places,i,j,k)){
-                            check=false;
-                            break;
-                        }   
+                        Queue<int[]> q=new LinkedList<>();
+                        boolean[][] visited=new boolean[5][5];
+                        q.offer(new int[]{j,k});
+                        visited[j][k]=true;
+                        
+                        while(!q.isEmpty()){
+                            int[] arr=q.poll();
+                            for(int d=0;d<4;d++){
+                                int nr=arr[0]+dr[d];
+                                int nc=arr[1]+dc[d];
+                                if(nr<0||nr>4||nc<0||nc>4)  continue;
+                                if(visited[nr][nc]||places[i][nr].charAt(nc)=='X'||(Math.abs(nr-j)+(Math.abs(nc-k)))>2)   continue;
+                                if(places[i][nr].charAt(nc)=='P'&&((Math.abs(nr-j)+(Math.abs(nc-k)))<=2)){
+                                    answer[i]=0;
+                                    break;
+                                }
+                                q.offer(new int[]{nr,nc});
+                                visited[nr][nc]=true;
+                            }
+                        }
                     }
+                    if(answer[i]==0)    break;
                 }
-                if(!check)  break;
+                if(answer[i]==0)    break;
             }
-            if(check)   answer[i]=1;
         }
         return answer;
-    }
-    public boolean distance(String[][] places,int idx,int r,int c){
-        int[] dr={-1,1,0,0};
-        int[] dc={0,0,-1,1};
-        Queue<int[]> q=new LinkedList<>();
-        boolean[][] visited=new boolean[5][5];
-        
-        q.offer(new int[]{r,c});
-        visited[r][c]=true;
-
-        while(!q.isEmpty()){
-            int[] arr=q.poll();
-
-            for(int i=0;i<4;i++){
-                int nr=arr[0]+dr[i];
-                int nc=arr[1]+dc[i];
-                if(nr<0||nr>4||nc<0||nc>4)  continue;
-                if(visited[nr][nc]||places[idx][nr].charAt(nc)=='X'||(Math.abs(r-nr)+Math.abs(c-nc))>2)  continue;
-                if(places[idx][nr].charAt(nc)=='P') return false;
-                
-                q.offer(new int[]{nr,nc});
-                visited[nr][nc]=true;
-            }
-        }
-        return true;
     }
 }
