01f005df15acc4b947ed36ce72a970ecb6aa12bc
dyeonss <dyeon7310@gmail.com>
2024-02-17 14:05:33 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.38 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,38 +1,28 @@
 import java.util.*;
 class Solution {
     public int solution(int[] cards) {
-        int answer = 0;
         ArrayList<Integer> box=new ArrayList<>();
-        HashSet<String> set=new HashSet<>();
+        boolean[] visited=new boolean[cards.length];
         
         for(int i=0;i<cards.length;i++){
-            boolean[] visited=new boolean[cards.length];
-            ArrayList<String> list=new ArrayList<>();
-            int select=cards[i];
-            list.add(String.valueOf(cards[i]));
-            visited[i]=true;
-            
-            while(!visited[select-1]){
-                list.add(String.valueOf(cards[select-1]));
-                visited[select-1]=true;
-                select=cards[select-1];
-            }
-            
-            Collections.sort(list);
-            
-            if(!set.contains(list.toString())){
-                set.add(list.toString());
-                if(list.size()==cards.length){
-                    box.add(0);
-                    continue;
-                }    
-                box.add(list.size());  
+            if(!visited[i]){
+                int cnt=1;
+                int select=cards[i];
+                visited[i]=true;
+
+                while(!visited[select-1]){
+                    visited[select-1]=true;
+                    select=cards[select-1];
+                    cnt++;
+                }
+                if(cnt==cards.length)   box.add(0);
+                else    box.add(cnt);
             }
         }
         
         box.sort(Collections.reverseOrder());
         if(box.size()>=2)   return box.get(0)*box.get(1);
         
-        return box.get(0);
+        return 0;
     }
 }
