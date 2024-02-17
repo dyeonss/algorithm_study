fc9be89365ee4c6a0469d0b4e7f06bb785899f07
dyeonss <dyeon7310@gmail.com>
2024-02-17 13:18:09 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 1.87 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -2,36 +2,37 @@ import java.util.*;
 class Solution {
     public int solution(int[] cards) {
         int answer = 0;
-        ArrayList<Integer> list=new ArrayList<>();
+        ArrayList<Integer> box=new ArrayList<>();
         HashSet<String> set=new HashSet<>();
         
         for(int i=0;i<cards.length;i++){
             boolean[] visited=new boolean[cards.length];
-            ArrayList<String> box=new ArrayList<>();
+            ArrayList<String> list=new ArrayList<>();
             int select=cards[i];
-            box.add(String.valueOf(cards[i]));
+            list.add(String.valueOf(cards[i]));
             visited[i]=true;
             
             while(!visited[select-1]){
-                box.add(String.valueOf(cards[select-1]));
+                list.add(String.valueOf(cards[select-1]));
                 visited[select-1]=true;
                 select=cards[select-1];
             }
             
-            Collections.sort(box);
+            Collections.sort(list);
             
-            if(!set.contains(box.toString())){
-                if(box.size()==cards.length)    list.add(0);
-                set.add(box.toString());
-                list.add(box.size());
+            if(!set.contains(list.toString())){
+                set.add(list.toString());
+                if(list.size()==cards.length){
+                    box.add(0);
+                    continue;
+                }    
+                box.add(list.size());  
             }
-            
         }
         
-        Collections.sort(list,Collections.reverseOrder());
-
-        if(list.size()>=2)   return list.get(0)*list.get(1);
+        box.sort(Collections.reverseOrder());
+        if(box.size()>=2)   return box.get(0)*box.get(1);
         
-        return list.get(0);
+        return box.get(0);
     }
 }
