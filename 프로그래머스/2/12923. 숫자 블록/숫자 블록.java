d210240023e73b3162153ad4b97cf03603d68f4e
dyeonss <dyeon7310@gmail.com>
2024-02-26 16:49:32 +0900

    [level 2] Title: 숫자 블록, Time: 229.58 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(long begin, long end) {
+        int[] answer = new int[(int)(end-begin)+1];
+        
+        for(long i=begin;i<=end;i++){
+            if(begin==1&&i-begin==0)  continue;
+            
+            
+            for(int j=2;j<=(int)Math.sqrt(i);j++){
+                if(i%j==0){
+                    if(i/j>10000000){
+                        answer[(int)(i-begin)]=j;
+                    }
+                    else{
+                        answer[(int)(i-begin)]=(int)(i/j);
+                        break;    
+                    }
+                }  
+            }    
+            
+            
+            if(answer[(int)(i-begin)]==0)  answer[(int)(i-begin)]=1;
+        }
+        return answer;
+    }
+}
