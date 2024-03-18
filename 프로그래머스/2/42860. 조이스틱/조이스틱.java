a34ae2b7995e27e684fc64dde48f4bcb485eeae6
dyeonss <dyeon7310@gmail.com>
2024-03-18 23:04:35 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,19 +1,17 @@
 class Solution {
     public int solution(String name) {
-        int answer = name.length()-1;
+        int move=name.length();
+        int answer=0;
         
-        for(int i=0;i<name.length()-1;i++){
+        for(int i=0;i<name.length();i++){
             int idx=i+1;
-            if(name.charAt(idx)=='A'){
-                while(idx<name.length()&&name.charAt(idx)=='A')  idx++;
-                answer=Math.min(answer,i*2+name.length()-idx);
-                answer=Math.min(answer,(name.length()-idx)*2+i);
-            }
-        }
-        
-        for(int i=0;i<name.length();i++)
             answer+=name.charAt(i)<='M'?name.charAt(i)-'A':'Z'-name.charAt(i)+1;
-        
+            while(idx<name.length()&&name.charAt(idx)=='A')  idx++;
+                move=Math.min(move,i*2+name.length()-idx);
+                move=Math.min(move,(name.length()-idx)*2+i);
+        }
+            
+        answer+=move;
         return answer;
     }
 }
