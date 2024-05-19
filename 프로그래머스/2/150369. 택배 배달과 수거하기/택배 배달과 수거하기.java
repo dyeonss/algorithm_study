427044abfad2811781b17b80d74707a5e500e6c8
dyeonss <dyeon7310@gmail.com>
2024-05-19 16:05:09 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 4.84 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,38 +1,21 @@
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        int d=0, p=0, idx=n-1, d_idx=0, p_idx=0;
+        //트럭 하나로 모든 배달과 수거를 마치고 물류창고까지 돌아올 수 있는 최소 이동 거리를 return 
         
-        while(idx>=0){
-            if(deliveries[idx]!=0||pickups[idx]!=0) answer+=(idx+1)*2;
-
-            for(d_idx=idx;d_idx>=0;d_idx--){
-                d+=deliveries[d_idx];
-                if(d<=cap){
-                    deliveries[d_idx]=0;
-                }
-                else if(d>cap){
-                    deliveries[d_idx]-=(cap-(d-deliveries[d_idx]));
-                    break;
-                }
-            }
+        int d = 0;
+        int p = 0;
+        for(int i=n-1; i>=0; i--){
+            d -= deliveries[i];
+            p -= pickups[i];
             
-            for(p_idx=idx;p_idx>=0;p_idx--){
-                p+=pickups[p_idx];
-                if(p<=cap){
-                    pickups[p_idx]=0;
-                }
-                else if(p>cap){
-                    pickups[p_idx]-=(cap-(p-pickups[p_idx]));
-                    break;
-                }
+            while(d < 0 || p < 0){
+                d += cap;
+                p += cap;
+                answer += (i+1)*2;
             }
-            
-            d=0;
-            p=0;
-          
-            idx=Math.max(d_idx,p_idx);
         }
+        
         return answer;
     }
 }
