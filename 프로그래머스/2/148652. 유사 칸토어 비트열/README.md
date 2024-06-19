3292a88798df564f2fa2b2a5e7e4b69083909814
dyeonss <dyeon7310@gmail.com>
2024-06-19 15:05:10 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 0.04 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/README.md
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/README.md
@@ -1,10 +1,10 @@
 # [level 2] 유사 칸토어 비트열 - 148652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148652) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148652#) 
 
 ### 성능 요약
 
-메모리: 74.7 MB, 시간: 0.03 ms
+메모리: 83 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 19일 14:52:54
+2024년 06월 19일 15:05:08
 
 ### 문제 설명
 
--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -2,41 +2,28 @@ import java.util.*;
 class Solution {
     public int solution(int n, long l, long r) {
         int answer = 0;
-        long len=(long)Math.pow(5,n-1), d=(long)Math.pow(4,n-1);
-        int count=0, cpn=n;
         
-        while(r!=0&&cpn>0){
-            count+=(d*((r/len)>2?(r/len)-1:(r/len)));
+        answer=countOne(n,r)-countOne(n,l-1);
+        
+        return answer;
+    }
+    public int countOne(int n, long l){
+        long len=(long)Math.pow(5,n-1), d=(long)Math.pow(4,n-1);
+        int count=0;
+        
+        while(n>0){
+            count+=(d*((l/len)>2?(l/len)-1:(l/len)));
             
-            if((r/len==2&&r%len!=0)||(r/len==3&&r%len==0)){
+            if((l/len==2&&l%len!=0)||(l/len==3&&l%len==0)){
                 break;
             }
             
-            cpn--;
-            r=r%len;
-            d=(long)Math.pow(4,cpn-1);
-            len=(long)Math.pow(5,cpn-1);
-        }
-        
-        answer+=count;
-        l--;
-        count=0;
-        len=(long)Math.pow(5,n-1);
-        d=(long)Math.pow(4,n-1);
-        
-        while(l!=0&&n>0){
-            count+=(d*((l/len)>2?(l/len)-1:(l/len)));
-            
-            if((l/len==2&&l%len!=0)||(l/len==3&&l%len==0))
-                break;
-            
             n--;
-            l=l%len;
+            l%=len;
             d=(long)Math.pow(4,n-1);
             len=(long)Math.pow(5,n-1);
         }
         
-        answer-=count;
-        return answer;
+        return count;
     }
 }
