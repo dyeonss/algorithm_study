c97e8d3ad2fb4e7e9da4a573d7ca757af8b9ce4a
dyeonss <dyeon7310@gmail.com>
2024-08-18 02:36:43 +0900

    [level 1] Title: 약수의 합, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,12 +1,8 @@
-import math
 def solution(n):
     answer = 0
 
-    for i in range(1,int(math.sqrt(n))+1):
+    for i in range(1,(n//2)+1):
         if n%i==0:
-            if i==n/i:
-                answer+=i
-            else:
-                answer+=(i+(n/i))
+            answer+=i
             
-    return answer
+    return answer+n
