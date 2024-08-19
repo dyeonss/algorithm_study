b4a54c576e4244557b980ceeaef4b7801811cb6d
dyeonss <dyeon7310@gmail.com>
2024-08-19 20:49:06 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,8 +1,5 @@
 def solution(seoul):
     answer = '김서방은 '
     
-    for idx, s in enumerate(seoul):
-        if s.find('Kim')!=-1:
-            answer+=str(idx)+'에 있다'
-            break
+    answer+=str(seoul.index('Kim'))+'에 있다'
     return answer
