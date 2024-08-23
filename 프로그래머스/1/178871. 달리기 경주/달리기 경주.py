3a1241c9f487d53e60fd993ffc7cee7ad4e034a2
dyeonss <dyeon7310@gmail.com>
2024-08-23 21:38:07 +0900

    [level 1] Title: 달리기 경주, Time: 678.17 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,14 @@
+def solution(players, callings):
+    running={}
+    
+    for idx, player in enumerate(players):
+        running[player]=idx
+    
+    for c in callings:
+        prev=running[c]
+        running[c]=prev-1
+        running[players[prev-1]]=prev
+        players[prev]=players[prev-1]
+        players[prev-1]=c
+        
+    return players
