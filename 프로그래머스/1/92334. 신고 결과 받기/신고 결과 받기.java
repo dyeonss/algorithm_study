0e9453568636b10d73941303da29f6fe0b7cad98
dyeonss <dyeon7310@gmail.com>
2024-08-23 20:14:31 +0900

    [level 1] Title: 신고 결과 받기, Time: 1443.41 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,14 +1,13 @@
 import java.util.*;
-// import java.util.stream.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int [id_list.length];
         HashMap<String, ArrayList<String>> map = new HashMap();
-        HashMap<String, Integer> singo = new HashMap();
+        HashMap<String, Integer> complain = new HashMap();
         
         for(String id: id_list){
             map.put(id, new ArrayList<String>());
-            singo.put(id, 0);
+            complain.put(id, 0);
         }
         
         report = Arrays.stream(report).distinct().toArray(String[]::new);
@@ -17,12 +16,12 @@ class Solution {
             String [] sarr = report[i].split(" ");
             if(!map.get(sarr[0]).contains(sarr[1])){
                 map.get(sarr[0]).add(sarr[1]);
-                singo.put(sarr[1], singo.get(sarr[1])+1);
+                complain.put(sarr[1], complain.get(sarr[1])+1);
             }
         }
         
         for(String id: id_list){
-            if(singo.get(id)>=k){
+            if(complain.get(id)>=k){
                 for(int j=0;j<id_list.length;j++){
                     if(map.get(id_list[j]).indexOf(id)!=-1){
                         answer[j]++;
