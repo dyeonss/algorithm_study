b0e9154037cbf8f5c114ede67ff79f56a010368e
dyeonss <dyeon7310@gmail.com>
2024-08-26 21:14:48 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -4,9 +4,7 @@ def solution(today, terms, privacies):
     today=int(year)*12*28+int(month)*28+int(day)
     info={}
     
-    for term in terms:
-        t,p=term.split()
-        info[t]=int(p)
+    info={t[0]: int(t[2:]) for t in terms}
     
     for i,privacy in enumerate(privacies):
         y,m,d=privacy[:10].split('.')
