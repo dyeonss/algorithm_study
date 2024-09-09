2643be67adb88dc837fddc2e757502a60a53920c
dyeonss <dyeon7310@gmail.com>
2024-09-09 22:09:23 +0900

    [level 2] Title: 짝지어 제거하기, Time: 143.27 ms, Memory: 13.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    stack=[]
+    
+    for i in s[::-1]:
+        if len(stack)>0 and stack[-1]==i:
+            stack.pop()
+        else:
+            stack.append(i)
+
+    return 1 if len(stack)==0 else 0
