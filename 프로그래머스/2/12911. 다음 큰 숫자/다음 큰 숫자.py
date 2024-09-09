7f765ecb99618ab0c2550ccfe34c0dfd93eb7908
dyeonss <dyeon7310@gmail.com>
2024-09-09 19:26:49 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer=n+1
+    cnt=bin(n)[2:].count('1')
+    
+    while True:
+        if cnt==bin(answer)[2:].count('1'):
+            break
+        answer+=1
+    return answer
