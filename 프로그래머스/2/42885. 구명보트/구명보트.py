2457c8c3d655992431377b4aa150df87a0d44709
dyeonss <dyeon7310@gmail.com>
2024-09-12 13:01:40 +0900

    [level 2] Title: 구명보트, Time: 9.05 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,16 @@
+def solution(people, limit):
+    answer = 0
+    start,end=0,len(people)-1
+    people.sort()
+    
+    while True:
+        if end<start:
+            break
+        if people[start]+people[end]<=limit:
+            answer+=1
+            start,end=start+1,end-1
+        else:
+            answer+=1
+            end-=1
+            
+    return answer
