2996f5096d8641a22ecd9ff0c5520cc867061791
dyeonss <dyeon7310@gmail.com>
2024-09-16 02:33:34 +0900

    [level 2] Title: [3차] 방금그곡, Time: 19.00 ms, Memory: 92.7 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,39 +1,41 @@
 import java.util.*;
 class Solution {
+    class Music{
+        int n,playTime;
+        String name;
+        public Music(int n,int playTime,String name){
+            this.n=n;
+            this.playTime=playTime;
+            this.name=name;
+        }
+    }
     public String solution(String m, String[] musicinfos) {
-        String answer = "(None)";
-        int maxTime=-1;
-        int[] times=new int[musicinfos.length];
-        
-        m=m.replace("C#","H");
-        m=m.replace("D#","I");
-        m=m.replace("F#","J");
-        m=m.replace("G#","K");
-        m=m.replace("A#","L");
+        String answer = "";
+        ArrayList<Music> list=new ArrayList<>();
+        m=replace(m);
         
         for(int i=0;i<musicinfos.length;i++){
-            String[] info=musicinfos[i].split(",");
-            int start=Integer.valueOf(info[0].substring(0,2))*60+Integer.valueOf(info[0].substring(3));
-            int end=Integer.valueOf(info[1].substring(0,2))*60+Integer.valueOf(info[1].substring(3));
-            times[i]=end-start;
-            StringBuilder tmp=new StringBuilder();
-            
-            info[3]=info[3].replace("C#","H");
-            info[3]=info[3].replace("D#","I");
-            info[3]=info[3].replace("F#","J");
-            info[3]=info[3].replace("G#","K");
-            info[3]=info[3].replace("A#","L");
-            char[] ch=info[3].toCharArray();
-            
-            for(int j=0;j<times[i];j++)
-                tmp.append(String.valueOf(ch[j%ch.length]));
-     
-            if(tmp.toString().contains(m)&&times[i]>maxTime){
-                answer=info[2];
-                maxTime=times[i];
-            }
+            String[] arr=musicinfos[i].split(",");
+            int time=(Integer.valueOf(arr[1].substring(0,2))*60+Integer.valueOf(arr[1].substring(3)))-
+                (Integer.valueOf(arr[0].substring(0,2))*60+Integer.valueOf(arr[0].substring(3)));
+            char[] ch=replace(arr[3]).toCharArray();
+            String s="";
+            for(int j=0;j<time+1;j++)
+                s+=String.valueOf(ch[j%ch.length]);
+            if(s.contains(m))   list.add(new Music(i,time,arr[2]));
         }
-
-        return answer;
+        list.sort((o1,o2)->{if(o2.playTime==o1.playTime)    return o1.n-o2.n;
+                            else    return o2.playTime-o1.playTime;});
+        
+        return list.size()>0?list.get(0).name:"(None)";
+    }
+    public String replace(String s){
+        s=s.replace("A#","H");
+        s=s.replace("B#","I");
+        s=s.replace("C#","J");
+        s=s.replace("D#","K");
+        s=s.replace("F#","L");
+        s=s.replace("G#","M");
+        return s;
     }
 }
