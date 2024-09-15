204c0c759b7cebd5364ba7765152fcac8f13da32
dyeonss <dyeon7310@gmail.com>
2024-09-15 17:04:23 +0900

    [level 2] Title: 배달, Time: 6.67 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,42 +1,26 @@
 import java.util.*;
 class Solution {
-    public class Node{
-        int n,v,d;
-        public Node(int n, int v, int d){
-            this.n=n;
-            this.v=v;
-            this.d=d;
-        }
-    }
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        ArrayList<ArrayList<Node>> conn=new ArrayList<>();
-        int[] dist=new int[N+1];
+        int[][] village=new int[N][N];
         
-        for(int i=0;i<N+1;i++)    conn.add(new ArrayList<>());
+        for(int i=0;i<N;i++){
+            Arrays.fill(village[i],K+1);
+            village[i][i]=0;
+        }
         
         for(int i=0;i<road.length;i++){
-            conn.get(road[i][0]).add(new Node(road[i][0],road[i][1],road[i][2]));
-            conn.get(road[i][1]).add(new Node(road[i][1],road[i][0],road[i][2]));
+            village[road[i][0]-1][road[i][1]-1]=Math.min(village[road[i][0]-1][road[i][1]-1],road[i][2]);
+            village[road[i][1]-1][road[i][0]-1]=Math.min(village[road[i][1]-1][road[i][0]-1],road[i][2]);
         }
+    
+        for(int i=0;i<N;i++)
+            for(int j=0;j<N;j++)
+                for(int k=0;k<N;k++)
+                    village[j][k]=Math.min(village[j][k],village[j][i]+village[i][k]);
+          
+        for(int i=0;i<N;i++)    if(village[0][i]<=K) answer++;
         
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[1]=0;
-        
-        PriorityQueue<Node> pq=new PriorityQueue<>((o1,o2)->{return o1.d-o2.d;});
-        
-        pq.addAll(conn.get(1));
-        
-        while(!pq.isEmpty()){
-            Node n=pq.poll();
-            if(n.d+dist[n.n]>dist[n.v]) continue;
-            dist[n.v]=n.d+dist[n.n];
-            pq.addAll(conn.get(n.v));
-        }
-        
-        for(int i=1;i<N+1;i++){
-            if(dist[i]<=K)  answer++;
-        }
         return answer;
     }
 }
