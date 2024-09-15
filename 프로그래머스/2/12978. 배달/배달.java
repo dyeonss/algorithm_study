2a8abe806fe7f3b4e8d47b98b7cc1b2e6231d8e0
dyeonss <dyeon7310@gmail.com>
2024-09-15 18:16:24 +0900

    [level 2] Title: 배달, Time: 1.99 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -3,23 +3,39 @@ class Solution {
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
         int[][] village=new int[N][N];
+        int[] minDist=new int[N];
+        LinkedList<int[]> q=new LinkedList<>();
+        Arrays.fill(minDist,Integer.MAX_VALUE);
+        minDist[0]=0;
         
-        for(int i=0;i<N;i++){
-            Arrays.fill(village[i],K+1);
-            village[i][i]=0;
-        }
         
         for(int i=0;i<road.length;i++){
-            village[road[i][0]-1][road[i][1]-1]=Math.min(village[road[i][0]-1][road[i][1]-1],road[i][2]);
-            village[road[i][1]-1][road[i][0]-1]=Math.min(village[road[i][1]-1][road[i][0]-1],road[i][2]);
+            int d=village[road[i][0]-1][road[i][1]-1];
+            village[road[i][0]-1][road[i][1]-1]=Math.min(d==0?Integer.MAX_VALUE:d,road[i][2]);
+            village[road[i][1]-1][road[i][0]-1]=Math.min(village[road[i][0]-1][road[i][1]-1],road[i][2]);
         }
-    
-        for(int i=0;i<N;i++)
-            for(int j=0;j<N;j++)
-                for(int k=0;k<N;k++)
-                    village[j][k]=Math.min(village[j][k],village[j][i]+village[i][k]);
-          
-        for(int i=0;i<N;i++)    if(village[0][i]<=K) answer++;
+        
+        for(int i=0;i<village[0].length;i++){
+            if(village[0][i]!=0){
+                minDist[i]=village[0][i];
+                q.offer(new int[]{i,minDist[i]});
+            }
+        }    
+
+        while(!q.isEmpty()){
+            int [] arr=q.poll();
+            
+            for(int i=0;i<village[arr[0]].length;i++){
+                if(village[arr[0]][i]!=0){
+                    if(arr[1]+village[arr[0]][i]<minDist[i]){
+                        minDist[i]=arr[1]+village[arr[0]][i];
+                        q.offer(new int[]{i,minDist[i]});
+                    }
+                }
+            }
+        }
+        
+        for(int i=0;i<N;i++)    if(minDist[i]<=K)   answer++;
         
         return answer;
     }
