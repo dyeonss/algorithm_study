69c9284f6d5583b8a2f3831e46b02058600511a2
dyeonss <dyeon7310@gmail.com>
2024-09-16 16:12:32 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.15 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,37 +1,57 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[][] places) {
-        int[] answer = new int[places.length];
-        int[] dr={-1,1,0,0};
-        int[] dc={0,0,-1,1};
+        int[] answer = new int[5];
+        Arrays.fill(answer,1);
         
-        for(int i=0;i<places.length;i++){
-            answer[i]=1;
-            for(int j=0;j<places[i].length;j++){
-                for(int k=0;k<places[i][j].length();k++){
+        for(int i=0;i<5;i++){
+            ArrayList<int[]> list=new ArrayList<>();
+            for(int j=0;j<5;j++){
+                for(int k=0;k<5;k++){
                     if(places[i][j].charAt(k)=='P'){
-                        Queue<int[]> q=new LinkedList<>();
-                        boolean[][] visited=new boolean[5][5];
-                        q.offer(new int[]{j,k});
-                        visited[j][k]=true;
-                        
-                        while(!q.isEmpty()){
-                            int[] arr=q.poll();
-                            for(int d=0;d<4;d++){
-                                int nr=arr[0]+dr[d];
-                                int nc=arr[1]+dc[d];
-                                if(nr<0||nr>4||nc<0||nc>4)  continue;
-                                if(visited[nr][nc]||places[i][nr].charAt(nc)=='X'||(Math.abs(nr-j)+(Math.abs(nc-k)))>2)   continue;
-                                if(places[i][nr].charAt(nc)=='P'&&((Math.abs(nr-j)+(Math.abs(nc-k)))<=2)){
-                                    answer[i]=0;
-                                    break;
-                                }
-                                q.offer(new int[]{nr,nc});
-                                visited[nr][nc]=true;
+                        list.add(new int[]{j,k});
+                    }
+                }
+            }
+        
+            for(int j=0;j<list.size()-1;j++){
+                for(int k=j+1;k<list.size();k++){
+                    int[] p1=list.get(j);
+                    int[] p2=list.get(k);
+                    int d=Math.abs(p1[0]-p2[0])+Math.abs(p1[1]-p2[1]);
+                    if(d==2){
+                        //같은 행에 존재할 때
+                        if(p1[0]==p2[0]){
+                            if(places[i][p1[0]].charAt((p1[1]+p2[1])/2)=='O'){
+                                answer[i]=0;
+                                break;
                             }
                         }
+                        //같은 열에 존재할 때
+                        else if(p1[1]==p2[1]){
+                            if(places[i][(p1[0]+p2[0])/2].charAt(p1[1])=='O'){
+                                answer[i]=0;
+                                break;
+                            }
+                        }
+                        //다른 행,열에 존재할 때
+                        else{
+                            int sr=Math.min(p1[0],p2[0]), er=Math.max(p1[0],p2[0]);
+                            int sc=Math.min(p1[1],p2[1]), ec=Math.max(p1[1],p2[1]);
+                            for(int r=sr;r<=er;r++)
+                                for(int c=sc;c<=ec;c++){
+                                    if(places[i][r].charAt(c)=='O'){
+                                        answer[i]=0;
+                                        break;
+                                    }
+                                }
+                                if(answer[i]==0)    break;
+                        }
                     }
-                    if(answer[i]==0)    break;
+                    else if(d==1){
+                        answer[i]=0;
+                        break;
+                    }   
                 }
                 if(answer[i]==0)    break;
             }
