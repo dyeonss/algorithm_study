d4611aa0786854c95fa5486427b1ed9449ea8793
dyeonss <dyeon7310@gmail.com>
2024-09-16 15:25:42 +0900

    [level 2] Title: 요격 시스템, Time: 445.82 ms, Memory: 185 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -2,15 +2,19 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
-        int val=-1;
+        int end=0;
         Arrays.sort(targets,(o1,o2)->o1[1]-o2[1]);
+        end=targets[0][1];
         
-        for(int[] t:targets){
-            if(val<t[0]){
-                val=t[1]-1;
-                answer++;
+        for(int i=0;i<targets.length-1;i++){
+            if(end>targets[i+1][0]){
             }
+            else{
+                end=targets[i+1][1];
+                answer++;
+            }    
         }
-        return answer;
+            
+        return answer+1;
     }
 }
