55a29ce8297a2cd1d165f99ef8bb27a184015d18
dyeonss <dyeon7310@gmail.com>
2024-09-20 00:13:40 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.25 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,11 +1,10 @@
 def solution(data, ext, val_ext, sort_by):
     answer = []
-    typ={"code":0,"date":1,"maximum":2,"remain":3}
+    map={"code":0,"date":1,"maximum":2,"remain":3}
     
-    for d in data:
-        if(d[typ[ext]]<val_ext):
-            answer.append(d)
+    for i in range(0,len(data)):
+        if data[i][map[ext]]<val_ext:
+            answer.append(data[i])
     
-    answer.sort(key=lambda x:x[typ[sort_by]])
-    
+    answer.sort(key=lambda x:x[map[sort_by]])
     return answer
