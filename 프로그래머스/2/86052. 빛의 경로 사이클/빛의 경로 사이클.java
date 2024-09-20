2bdaeb84cba51f876f48757f5c6337b76c55ee4c
dyeonss <dyeon7310@gmail.com>
2024-09-20 14:04:14 +0900

    [level 2] Title: 빛의 경로 사이클, Time: 74.21 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
+++ 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
@@ -3,28 +3,29 @@ class Solution {
     public int[] solution(String[] grid) {
         int[] answer={};
         ArrayList<Integer> list=new ArrayList<>();
-        boolean[][][] visited=new boolean[grid.length][grid[0].length()][4];
+        int r=grid.length, c=grid[0].length();
+        boolean[][][] visited=new boolean[r][c][4];
         int[] dr={1,0,-1,0}, dc={0,1,0,-1};
         
-        for(int i=0;i<visited.length;i++){
-            for(int j=0;j<visited[0].length;j++){
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
                 for(int k=0;k<4;k++){   //아래 오 위 왼
                     if(!visited[i][j][k]){
                         int len=0;
-                        int r=i,c=j,d=k;
+                        int nr=i,nc=j,d=k;
                         visited[i][j][k]=true;
                         while(true){
-                            if(grid[r].charAt(c)=='L')
+                            if(grid[nr].charAt(nc)=='L')
                                 d=(d+1)%4;
                             
-                            else if(grid[r].charAt(c)=='R')
+                            else if(grid[nr].charAt(nc)=='R')
                                 d=(d+3)%4;
                             
-                            r=(visited.length+r+dr[d])%visited.length;
-                            c=(visited[0].length+c+dc[d])%visited[0].length;
-                            visited[r][c][d]=true;
+                            nr=(r+nr+dr[d])%r;
+                            nc=(c+nc+dc[d])%c;
+                            visited[nr][nc][d]=true;
                             len++;
-                            if(r==i&&c==j&&d==k)  break;
+                            if(nr==i&&nc==j&&d==k)  break;
                         }
                         list.add(len);
                     }
