60b5f59acbff03d7e1b06b809ccf6c57205a7a19
dyeonss <dyeon7310@gmail.com>
2024-09-29 16:42:20 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 18892 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,49 +2,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int result;
-    static boolean [] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st= new StringTokenizer(br.readLine());
-        int N=Integer.parseInt(st.nextToken());
-        int K=Integer.parseInt(st.nextToken());
-        visited=new boolean[100001];
-        bfs(N,K);
-        System.out.println(result);
-    }
-
-    private static void bfs(int n, int k) {
-        Queue<Integer> q=new LinkedList<>();
-
-        q.offer(n);
+        int n=Integer.parseInt(st.nextToken());
+        int k=Integer.parseInt(st.nextToken());
+        boolean[] visited=new boolean[100001];
+        LinkedList<int[]> q=new LinkedList<>();
+        int result=0;
         visited[n]=true;
+        q.offer(new int[]{n,0});
 
-        while (!q.isEmpty()){
-            int len=q.size();
+        while(!q.isEmpty()){
+            int[] p=q.poll();
 
-            for (int i = 0; i < len; i++) {
-                int cur = q.poll();
-                if(cur==k)  return;
-
-                if(cur-1>=0&&cur-1<=100000&&!visited[cur-1]){
-                    q.offer(cur-1);
-                    visited[cur-1]=true;
-                }
-                if(cur+1>=0&&cur+1<=100000&&!visited[cur+1]){
-                    q.offer(cur+1);
-                    visited[cur+1]=true;
-                }
-                if(cur*2>=0&&cur*2<=100000&&!visited[cur*2]){
-                    q.offer(cur*2);
-                    visited[cur*2]=true;
-                }
+            if(p[0]==k){
+                result=p[1];
+                break;
             }
-            result++;
+            if(0<=p[0]-1&&p[0]-1<=100000&&!visited[p[0]-1]){
+                q.offer(new int[]{p[0]-1,p[1]+1});
+                visited[p[0]-1]=true;
+            }
+            if(0<=p[0]+1&&p[0]+1<=100000&&!visited[p[0]+1]){
+                q.offer(new int[]{p[0]+1,p[1]+1});
+                visited[p[0]+1]=true;
+            }
+            if(0<=p[0]*2&&p[0]*2<=100000&&!visited[p[0]*2]){
+                q.offer(new int[]{p[0]*2,p[1]+1});
+                visited[p[0]*2]=true;
+            }
         }
+        System.out.println(result);
     }
 }
