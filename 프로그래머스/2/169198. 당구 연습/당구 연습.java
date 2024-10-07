21f3442cf33a1d489b3a5e4d45310fcb5c0aa441
dyeonss <dyeon7310@gmail.com>
2024-10-08 02:46:14 +0900

    [level 2] Title: 당구 연습, Time: 0.44 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/당구 연습.java
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.java
@@ -3,20 +3,24 @@ class Solution {
         int[] answer = new int[balls.length];
         
         for(int i=0;i<balls.length;i++){
-            int x=balls[i][0], y=balls[i][1];
-            int distance=Integer.MAX_VALUE;
+            int x=balls[i][0], y=balls[i][1], distance=0;
+            int d1=(int)(Math.pow(startX-x,2)+Math.pow(startY-(n+(n-y)),2)),
+            d2=(int)(Math.pow(startX-x,2)+Math.pow(startY-(-y),2)),
+            d3=(int)(Math.pow(startX-(-x),2)+Math.pow(startY-y,2)),
+            d4=(int)(Math.pow(startX-(m+(m-x)),2)+Math.pow(startY-y,2));
             
-            if(!(startX==x&&y>startY))   //상
-                distance=Math.min(distance, (int)(Math.pow(startX-x,2)+Math.pow(startY-(n+(n-y)),2)));
-        
-            if(!(startX==x&&y<startY))  //하
-                distance=Math.min(distance, (int)(Math.pow(startX-x,2)+Math.pow(startY-(-y),2)));
-              
-            if(!(startY==y&&x<startX))  //좌
-                distance=Math.min(distance, (int)(Math.pow(startX-(-x),2)+Math.pow(startY-y,2)));
-             
-            if(!(startY==y&&x>startX))  //우
-                distance=Math.min(distance, (int)(Math.pow(startX-(m+(m-x)),2)+Math.pow(startY-y,2)));
+            if(startX==x){
+                distance=Math.min(d3,d4);
+                if(startY>y)    distance=Math.min(distance,(int)Math.pow(2*n-startY-y,2));
+                else if(startY<y)   distance=Math.min(distance,(int)Math.pow(startY+y,2));
+            }
+            else if(startY==y){
+                distance=Math.min(d1,d2);
+                if(startX>x)    distance=Math.min(distance,(int)Math.pow(2*m-startX-x,2));
+                else if(startX<x)   distance=Math.min(distance,(int)Math.pow(startX+x,2));
+            }
+            else
+                distance=Math.min(Math.min(d1,d2),Math.min(d3,d4));
              
             answer[i]=distance;
         }
