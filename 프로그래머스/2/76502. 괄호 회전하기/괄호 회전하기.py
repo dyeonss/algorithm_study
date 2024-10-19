4fed6e12cc6b239368c3d720a5f5ec7abc0b243e
dyeonss <dyeon7310@gmail.com>
2024-10-19 19:37:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,20 +1,17 @@
 def solution(s):
     answer = 0
-    tmp=s
-    for i in range(len(s)):
-        tmp=tmp[1:]+tmp[0]
+    for _ in range(len(s)):
         stack=[]
-        for i in range(len(tmp)):
+        for i in range(len(s)):
             if len(stack)==0:
-                stack.append(tmp[i])
+                stack.append(s[i])
             else:
-                t,last=tmp[i], stack[-1]
-                
+                t,last=s[i], stack[-1]
                 if (t==')' and last=='(') or (t=='}' and last=='{') or (t==']' and last=='['):
                     stack.pop()
                 else:
-                    stack.append(tmp[i])
+                    stack.append(s[i])
         if len(stack)==0:
             answer+=1
-        
+        s=s[1:]+s[0]
     return answer
